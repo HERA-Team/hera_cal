88bfea03caf87eae66bceef3943bac8f5f489c4f
Zaki Ali <zakiali88@gmail.com>
2017-01-20 14:13:09 -0800

    change shape of arrays in heracal to not be excessive.
    Some bug fixes in the paremter values.

--- heracal/heracal.py
+++ heracal/heracal.py
@@ -1,14 +1,17 @@
-from uvdata.calfits import CALFITS
+from uvdata.cal import UVCal
 import numpy as np
 
 
-class HERACal(CALFITS):
+class HERACal(UVCal):
     '''Class that loads in hera omnical data,'''
     def __init__(self, meta, gains, ex_ants = []):
         '''given meta and gain dictionary from omni_run.py before to_npz() call,
            to reshuffle data and populating the UVCal class.'''
         
         super(HERACal, self).__init__()
+    
+        str2pol = {'x': -5, 'y': -6}
+        pol2str = {-5: 'x', -6: 'y'}
         
         chisqdict = {}
         datadict = {}
@@ -24,8 +27,8 @@ class HERACal(CALFITS):
         ants = np.sort(ants)
         allants = np.sort(allants)
         time = meta['jds']
-        freq = meta['freqs']/1e6 # in GHz
-        pols = gains.keys(); npol = len(pol)
+        freq = meta['freqs'] # in GHz
+        pols = [ str2pol[p] for p in gains.keys() ]; npol = len(pols)
         ntimes = time.shape[0]
         nfreqs = freq.shape[0]
         nants = len(ants)
@@ -37,7 +40,7 @@ class HERACal(CALFITS):
             fl = []
             for ant in ants:
                 try: 
-                    dd.append(datadict[str(ant)+pols[ii]])
+                    dd.append(datadict[str(ant)+pol2str[pols[ii]]])
                     fl.append(np.zeros((ntimes,nfreqs),dtype=bool))
                 #if antenna not in data dict (aka, a bad antenna)
                 except(KeyError): 
@@ -45,6 +48,7 @@ class HERACal(CALFITS):
             datarray.append(dd)
             flgarray.append(fl)
 
+        '''
         datarray = np.array(datarray)
         datarray = datarray.swapaxes(0,3).swapaxes(0,1).reshape(nants*nfreqs*npol*ntimes)
 
@@ -62,26 +66,50 @@ class HERACal(CALFITS):
             ch = []
             for ant in ants:
                 try:
-                    ch.append(meta['chisq'+str(ant)+pols[ii]])
+                    ch.append(meta['chisq'+str(ant)+pol2str[pols[ii]]])
                 except:
                     ch.append(np.ones((ntimes,nfreqs)))
             chisqarray.append(ch)
         chisqarray = np.array(chisqarray).swapaxes(0,3).swapaxes(0,1).reshape(nants*nfreqs*npol*ntimes)
-        
+        '''
+        datarray = np.array(datarray)
+        datarray = datarray.swapaxes(0,3).swapaxes(0,1)
 
+        flgarray = np.array(flgarray)
+        flgarray = flgarray.swapaxes(0,3).swapaxes(0,1)
+        
+        tarray = time
+        parray = np.array(pols)
+        farray = np.array(freq)
+        numarray = np.array(list(ants))
+        namarray = np.array(antnames)
+
+        chisqarray = []
+        for ii in range(npol):
+            ch = []
+            for ant in ants:
+                try:
+                    ch.append(meta['chisq'+str(ant)+pol2str[pols[ii]]])
+                except:
+                    ch.append(np.ones((ntimes,nfreqs)))
+            chisqarray.append(ch)
+        chisqarray = np.array(chisqarray).swapaxes(0,3).swapaxes(0,1)
+
+        
+#       import IPython; IPython.embed()
         self.Nfreqs = nfreqs
         self.Npols = len(pols)
         self.Ntimes = ntimes
-        self.history = ''
+        self.history = 'Test file.'
         self.Nants_data = len(ants)  # only ants with data
-        self.antenna_names = namarray
-        self.antenna_numbers = numarray
+        self.antenna_names = namarray[:self.Nants_data]
+        self.antenna_numbers = numarray[:self.Nants_data]
         self.Nants_telescope = nants # total number of antennas
         self.Nspws = 1
 
-        self.freq_array = farray
-        self.polarization_array = parray
-        self.time_array = tarray
+        self.freq_array = farray[:self.Nfreqs].reshape(self.Nspws,-1)
+        self.polarization_array = parray[:self.Npols]
+        self.time_array = tarray[:self.Ntimes]
         self.gain_convention = 'divide'
         self.flag_array = flgarray
         self.quality_array = chisqarray  
