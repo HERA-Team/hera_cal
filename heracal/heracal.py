3cdbd28c7810c67fe78bb5f6e886f172e95f0215
Zaki Ali <zakiali88@gmail.com>
2017-02-21 11:51:06 -0800

    pep8 fixes. Remove commented code.

--- heracal/heracal.py
+++ heracal/heracal.py
@@ -4,31 +4,32 @@ import numpy as np
 
 class HERACal(UVCal):
     '''Class that loads in hera omnical data,'''
-    def __init__(self, meta, gains, DELAY=False, ex_ants = [], appendhist=''):
+    def __init__(self, meta, gains, DELAY=False, ex_ants=[], appendhist=''):
         '''given meta and gain dictionary from omni_run.py before to_npz() call,
            to reshuffle data and populating the UVCal class.'''
-        
+
         super(HERACal, self).__init__()
-    
+
         str2pol = {'x': -5, 'y': -6}
         pol2str = {-5: 'x', -6: 'y'}
-        
+
         chisqdict = {}
         datadict = {}
         ants = []
         for pol in gains:
             for ant in np.sort(gains[pol].keys()):
-                datadict['%d%s' %(ant,pol)] = gains[pol][ant]
-                if not ant in ants:
+                datadict['%d%s' % (ant, pol)] = gains[pol][ant]
+                if ant not in ants:
                     ants.append(ant)
 
-        # drop antennas that are not solved for. 
+        # drop antennas that are not solved for.
         allants = ants + ex_ants
         ants = np.sort(ants)
         allants = np.sort(allants)
         time = meta['jds']
-        freq = meta['freqs'] # in GHz
-        pols = [ str2pol[p] for p in gains.keys() ]; npol = len(pols)
+        freq = meta['freqs']  # in GHz
+        pols = [str2pol[p] for p in gains.keys()]
+        npol = len(pols)
         ntimes = time.shape[0]
         nfreqs = freq.shape[0]
         nants = len(ants)
@@ -39,21 +40,21 @@ class HERACal(UVCal):
             dd = []
             fl = []
             for ant in ants:
-                try: 
+                try:
                     dd.append(datadict[str(ant)+pol2str[pols[ii]]])
-                    fl.append(np.zeros_like(dd[-1],dtype=bool))
-                #if antenna not in data dict (aka, a bad antenna)
-                except(KeyError): 
+                    fl.append(np.zeros_like(dd[-1], dtype=bool))
+                # if antenna not in data dict (aka, a bad antenna)
+                except(KeyError):
                     print "Can't find antenna {0}".format(ant)
             datarray.append(dd)
             flgarray.append(fl)
 
         datarray = np.array(datarray)
-        datarray = datarray.swapaxes(0,3).swapaxes(0,1)
+        datarray = datarray.swapaxes(0, 3).swapaxes(0, 1)
 
         flgarray = np.array(flgarray)
-        flgarray = flgarray.swapaxes(0,3).swapaxes(0,1)
-        
+        flgarray = flgarray.swapaxes(0, 3).swapaxes(0, 1)
+
         tarray = time
         parray = np.array(pols)
         farray = np.array(freq)
@@ -67,42 +68,30 @@ class HERACal(UVCal):
                 try:
                     ch.append(meta['chisq'+str(ant)+pol2str[pols[ii]]])
                 except:
-                    ch.append(np.ones_like(dd[-1])) # array of ones like the data
+                    ch.append(np.ones_like(dd[-1]))  # array of ones
             chisqarray.append(ch)
-        chisqarray = np.array(chisqarray).swapaxes(0,3).swapaxes(0,1)
+        chisqarray = np.array(chisqarray).swapaxes(0, 3).swapaxes(0, 1)
 
-        
         self.Nfreqs = nfreqs
         self.Npols = len(pols)
         self.Ntimes = ntimes
         try:
             self.history = meta['history'].replace('\n', ' ') + appendhist
-        except KeyError: 
+        except KeyError:
             self.history = appendhist
         self.Nants_data = len(ants)  # only ants with data
         self.antenna_names = namarray[:self.Nants_data]
         self.antenna_numbers = numarray[:self.Nants_data]
-        self.Nants_telescope = nants # total number of antennas
+        self.Nants_telescope = nants  # total number of antennas
         self.Nspws = 1
 
-        self.freq_array = farray[:self.Nfreqs].reshape(self.Nspws,-1)
+        self.freq_array = farray[:self.Nfreqs].reshape(self.Nspws, -1)
         self.polarization_array = parray[:self.Npols]
         self.time_array = tarray[:self.Ntimes]
         self.gain_convention = 'divide'
         self.x_orientation = 'east'
         if DELAY:
             self.set_delay()
-            #dataarray = datarray.reshape(self.Nants_data, self.Ntimes, self.Npols)
-            # collapse on the second axis...should have dimension 1
-            #if datarray.shape[1] == 1: self.delay_array = datarray[:, 0, :, :]
-            #else: raise ValueError('Frequency dimension is not equal to 1. Please check \
-            #                        it is delay data.')
-            #if chisqarray.shape[1] == 1: self.quality_array = chisqarray[:, 0, :, :]
-            #else: raise ValueError('Frequency dimension is not equal to 1. Please check \
-            #                        it is delay data')
-            #if flgarray.shape[1] == 1:  self.flag_array = flgarray[:, 0, :, :]
-            #else: raise ValueError('Frequency dimension is not equal to 1. Please check \
-            #                        it is delay data.')
             self.delay_array = datarray
             self.quality_array = chisqarray
             self.flag_array = flgarray
@@ -112,5 +101,3 @@ class HERACal(UVCal):
             self.gain_array = datarray
             self.quality_array = chisqarray
             self.flag_array = flgarray
-
-
