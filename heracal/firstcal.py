082bec5218f178c13814d05342261e3fe31fa5fa
Zaki Ali <zakiali88@gmail.com>
2017-04-24 21:56:35 -0700

    update numpy as n to numpy as np in firstcal.py
    removed save_gains and get_phase and move compute_reds back to omni.py
    update imports

--- heracal/firstcal.py
+++ heracal/firstcal.py
@@ -2,25 +2,27 @@
 Tools for dealing with redundant array configurations.
 '''
 
-import numpy as n
+import numpy as np
 import aipy as a
 import pylab as p
 import time 
+import omnical
 import multiprocessing as mpr
+import scipy.sparse as sps
 
 def fit_line(phs, fqs, valid, offset=False):
     fqs = fqs.compress(valid)
     dly = phs.compress(valid)
-    B = n.zeros((fqs.size,1)); B[:,0] = dly
+    B = np.zeros((fqs.size,1)); B[:,0] = dly
     if offset:
-        A = n.zeros((fqs.size,2)); A[:,0] = fqs*2*n.pi; A[:,1] = 1
+        A = np.zeros((fqs.size,2)); A[:,0] = fqs*2*np.pi; A[:,1] = 1
     else:
-        A = n.zeros((fqs.size,1)); A[:,0] = fqs*2*n.pi#; A[:,1] = 1
+        A = np.zeros((fqs.size,1)); A[:,0] = fqs*2*np.pi#; A[:,1] = 1
     try:
         if offset:
-            dt,off = n.linalg.lstsq(A,B)[0].flatten()
+            dt,off = np.linalg.lstsq(A,B)[0].flatten()
         else:
-            dt = n.linalg.lstsq(A,B)[0][0][0]
+            dt = np.linalg.lstsq(A,B)[0][0][0]
             off = 0.0
         return dt,off
     except ValueError:
@@ -65,12 +67,12 @@ def redundant_bl_cal_simple(d1,w1,d2,w2,fqs, cleantol=1e-4, window='none', finet
             Array of offsets (if average == False), or single delay.
     
 '''
-    d12 = d2 * n.conj(d1)
+    d12 = d2 * np.conj(d1)
     # For 2D arrays, assume first axis is time. 
     if average:
         if d12.ndim > 1:
-            d12_sum = n.sum(d12,axis=0).reshape(1,-1)
-            d12_wgt = n.sum(w1*w1,axis=0).reshape(1,-1)
+            d12_sum = np.sum(d12,axis=0).reshape(1,-1)
+            d12_wgt = np.sum(w1*w1,axis=0).reshape(1,-1)
         else:
             d12_sum = d12.reshape(1,-1)
             d12_wgt = w1.reshape(1,-1)*w2.reshape(1,-1)
@@ -79,13 +81,13 @@ def redundant_bl_cal_simple(d1,w1,d2,w2,fqs, cleantol=1e-4, window='none', finet
         d12_wgt = w1*w2
     #normalize data to maximum so that we minimize fft articats from RFI
     d12_sum *= d12_wgt
-    d12_sum = d12_sum/n.where(n.abs(d12_sum)==0., 1., n.abs(d12_sum)) 
+    d12_sum = d12_sum/np.where(np.abs(d12_sum)==0., 1., np.abs(d12_sum)) 
     window = a.dsp.gen_window(d12_sum[0,:].size, window=window)
-    dlys = n.fft.fftfreq(fqs.size, fqs[1]-fqs[0])
+    dlys = np.fft.fftfreq(fqs.size, fqs[1]-fqs[0])
     # FFT and deconvolve the weights to get the phs
-    _phs = n.fft.fft(window*d12_sum,axis=-1)
-    _wgt = n.fft.fft(window*d12_wgt,axis=-1)
-    _phss = n.zeros_like(_phs)
+    _phs = np.fft.fft(window*d12_sum,axis=-1)
+    _wgt = np.fft.fft(window*d12_wgt,axis=-1)
+    _phss = np.zeros_like(_phs)
     if not noclean and average:
         _phss = a.deconv.clean(_phs, _wgt, tol=cleantol)[0]
     elif not noclean:
@@ -93,52 +95,52 @@ def redundant_bl_cal_simple(d1,w1,d2,w2,fqs, cleantol=1e-4, window='none', finet
         _phss = pool.map(mpr_clean, zip(_phs,_wgt))
     else:
         _phss = _phs
-    _phss = n.abs(_phss)
+    _phss = np.abs(_phss)
     #get bin of phase
-    mxs = n.argmax(_phss, axis=-1)
+    mxs = np.argmax(_phss, axis=-1)
     #Fine tune with linear fit.
     mxs[mxs>_phss.shape[-1]/2] -= _phss.shape[-1]
     dtau = mxs / (fqs[-1] - fqs[0])
-    mxs = n.dot(mxs.reshape(len(mxs),1),n.ones((1,3),dtype=int)) + n.array([-1,0,1])
-    taus = n.sum(_phss[n.arange(mxs.shape[0],dtype=int).reshape(-1,1),mxs] * dlys[mxs],axis=-1) / n.sum(_phss[n.arange(mxs.shape[0]).reshape(-1,1),mxs],axis=-1)
+    mxs = np.dot(mxs.reshape(len(mxs),1),np.ones((1,3),dtype=int)) + np.array([-1,0,1])
+    taus = np.sum(_phss[np.arange(mxs.shape[0],dtype=int).reshape(-1,1),mxs] * dlys[mxs],axis=-1) / np.sum(_phss[np.arange(mxs.shape[0]).reshape(-1,1),mxs],axis=-1)
     dts,offs = [],[]
     if finetune:
     #loop over the linear fits
         t1 = time.time()
         for ii,(tau,d) in enumerate(zip(taus,d12_sum)):
-            valid = n.where(d != 0, 1, 0) # Throw out zeros, which NaN in the log below
-            valid = n.logical_and(valid, n.logical_and(fqs>.11,fqs<.19))
-            dly = n.angle(d*n.exp(-2j*n.pi*tau*fqs))
+            valid = np.where(d != 0, 1, 0) # Throw out zeros, which NaN in the log below
+            valid = np.logical_and(valid, np.logical_and(fqs>.11,fqs<.19))
+            dly = np.angle(d*np.exp(-2j*np.pi*tau*fqs))
             dt,off = fit_line(dly,fqs,valid,offset=offset)
             dts.append(dt), offs.append(off)
             if plot:
                 p.subplot(411)
-                p.plot(fqs,n.angle(d12_sum[ii]), linewidth=2)
+                p.plot(fqs,np.angle(d12_sum[ii]), linewidth=2)
                 p.plot(fqs,d12_sum[ii], linewidth=2)
-                p.plot(fqs, n.exp((2j*n.pi*fqs*(tau+dt))+off))
-                p.hlines(n.pi, .1,.2,linestyles='--',colors='k')
-                p.hlines(-n.pi, .1,.2,linestyles='--',colors='k')
+                p.plot(fqs, np.exp((2j*np.pi*fqs*(tau+dt))+off))
+                p.hlines(np.pi, .1,.2,linestyles='--',colors='k')
+                p.hlines(-np.pi, .1,.2,linestyles='--',colors='k')
                 p.subplot(412)
-                p.plot(fqs,n.unwrap(dly)+2*n.pi*tau*fqs, linewidth=2)
-                p.plot(fqs,dly+2*n.pi*tau*fqs, linewidth=2,ls='--')
-                p.plot(fqs,2*n.pi*tau*fqs, linewidth=2,ls='-.')
-                p.plot(fqs,2*n.pi*(tau+dt)*fqs + off, linewidth=2,ls=':')
+                p.plot(fqs,np.unwrap(dly)+2*np.pi*tau*fqs, linewidth=2)
+                p.plot(fqs,dly+2*np.pi*tau*fqs, linewidth=2,ls='--')
+                p.plot(fqs,2*np.pi*tau*fqs, linewidth=2,ls='-.')
+                p.plot(fqs,2*np.pi*(tau+dt)*fqs + off, linewidth=2,ls=':')
                 p.subplot(413)
-                p.plot(dlys, n.abs(_phs[ii]),'-.')
+                p.plot(dlys, np.abs(_phs[ii]),'-.')
                 p.xlim(-400,400)
                 p.subplot(414)
                 p.plot(fqs,dly, linewidth=2)
-                p.plot(fqs,off+dt*fqs*2*n.pi, '--')
-                p.hlines(n.pi, .1,.2,linestyles='--',colors='k')
-                p.hlines(-n.pi, .1,.2,linestyles='--',colors='k')
+                p.plot(fqs,off+dt*fqs*2*np.pi, '--')
+                p.hlines(np.pi, .1,.2,linestyles='--',colors='k')
+                p.hlines(-np.pi, .1,.2,linestyles='--',colors='k')
                 print 'tau=', tau
                 print 'tau + dt=', tau+dt
                 p.xlabel('Frequency (GHz)', fontsize='large')
                 p.ylabel('Phase (radians)', fontsize='large')
         p.show()
 
-        dts = n.array(dts)
-        offs = n.array(offs)
+        dts = np.array(dts)
+        offs = np.array(offs)
 
     info = {'dtau':dts, 'off':offs, 'mx':mxs}
     if verbose: print info, taus, taus+dts, offs
@@ -158,14 +160,6 @@ class FirstCalRedundantInfo(omnical.info.FirstCalRedundantInfo):
         of (time,freq) data.''' # XXX does time/freq ordering matter.  should data be 2D instead?
         return np.array([dd[bl] if dd.has_key(bl) else dd[bl[::-1]].conj()
             for bl in self.bl_order()]).transpose((1,2,0))
-
-def compute_reds(nant, pols, *args, **kwargs):
-    _reds = omnical.arrayinfo.compute_reds(*args, **kwargs)
-    reds = []
-    for pi in pols:
-        for pj in pols:
-            reds += [[(Antpol(i, pi, nant), Antpol(j, pj, nant)) for i, j in gp] for gp in _reds]
-    return reds
 
 
 class FirstCal(object):
@@ -258,46 +252,3 @@ class FirstCal(object):
             dlys = self.xhat[ant_indexes]
             solved_delays.append(dlys[0] - dlys[1] - dlys[2] + dlys[3])
         self.solved_delays = np.array(solved_delays)
-
-# PYUV
-def get_phase(fqs,tau, offset=False):
-    fqs = fqs.reshape(-1,1) #need the extra axis
-    if offset:
-        delay = tau[0]
-        offset = tau[1]
-        return np.exp(-1j*(2*np.pi*fqs*delay) - offset)
-    else:
-        return np.exp(-2j*np.pi*fqs*tau)
-
-# PYUV
-def save_gains_fc(s,fqs,pol,filename,ubls=None,ex_ants=None,verbose=False):
-    """
-    s: solutions
-    fqs: frequencies
-    pol: polarization of single antenna i.e. 'x', or 'y'.
-    filename: if a specific file was used (instead of many), change output name
-    ubls: unique baselines used to solve for s'
-    ex_ants: antennae excluded to solve for s'
-    """
-#    if isinstance(filename, list): filename=filename[0] #XXX this is evil. why?
-    NBINS = len(fqs)
-    s2 = {}
-    for k,i in s.iteritems():
-        #len > 1 means that one is using the "tune" parameter in omni.firstcal
-        if len(i)>1:
-            s2[str(k)+pol] = get_phase(fqs,i,offset=True).T
-            s2['d'+str(k)] = i[0]
-            s2['o'+str(k)] = i[1]
-            if verbose: print 'dly=%f , off=%f'%i
-        else:
-            s2[str(k)+pol] = get_phase(fqs,i).T #reshape plays well with omni apply
-            s2['d'+str(k)] = i
-            if verbose: print 'dly=%f'%i
-    if not ubls is None: s2['ubls']=ubls
-    if not ex_ants is None: s2['ex_ants']=ex_ants
-    s2['freqs']=fqs#in GHz
-    outname='%s.fc.npz'%filename
-    import sys
-    s2['cmd'] = ' '.join(sys.argv)
-    print 'Saving fcgains to %s'%outname
-    np.savez(outname,**s2)
