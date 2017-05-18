ff2f6462b87090c6388c419f0e0fe67f43e0a603
Saul A. Kohn <saul.aryeh.kohn@gmail.com>
2017-05-18 16:30:50 -0400

    now applies gains properly when more than one polarization was supplied. Previously, only the 0th gain polarization in the gain_array axis was being applied. Also collectsfirstcal files appropriately -- but cannot apply more than one firstcal file until we can concatenate UVCal objects along their polarization axis. Working on that now.

--- scripts/omni_apply.py
+++ scripts/omni_apply.py
@@ -19,21 +19,51 @@ args = np.sort(args)
 pols = opts.pol.split(',')
 
 def file2pol(filename): return filename.split('.')[3] #XXX assumes file naming format
+def isLinPol(polstr): return len(list(set(polstr)))==1
+
+linear_pol_keys = []
+for pp in pols:
+    if isLinPol(pp):
+        linear_pol_keys.append(pp)
 
 filedict = {}
 solution_files = sorted(glob.glob(opts.omnipath))
 
+if opts.firstcal:
+    firstcal_files = {}
+    nf=0
+    for pp in pols:
+        if isLinPol(pp):
+            firstcal_files[pp] = sorted([s for s in glob.glob(opts.firstcal) if pp in s])
+            nf += len(firstcal_files[pp])    
+
 for i, f in enumerate(args):
     pp = file2pol(f)
-    if len(pols)==1:
-        fexpected = '%s/%s.fits'%(os.path.dirname(opts.omnipath),os.path.basename(f))
-    else: 
-        fexpected = '%s/%s.fits'%(os.path.dirname(opts.omnipath), os.path.basename(f).replace('.%s'%pp,''))
-    try:
-        ind = solution_files.index(fexpected)
-        filedict[f] = str(solution_files[ind])
-    except ValueError:
-       raise Exception('Solution file %s.fits expected; not found.'%f)
+    if not opts.firstcal:
+        if len(pols)==1:
+            fexpected = '%s/%s.fits'%(os.path.dirname(opts.omnipath),os.path.basename(f))
+        else: 
+            fexpected = '%s/%s.fits'%(os.path.dirname(opts.omnipath), os.path.basename(f).replace('.%s'%pp,''))
+        try:
+            ind = solution_files.index(fexpected)
+            filedict[f] = str(solution_files[ind])
+        except ValueError:
+           raise Exception('Solution file %s.fits expected; not found.'%f)
+    else:
+        if nf == len(solution_files)*len(pols): # atomic firstcal application
+            filedict[f] = solution_files[i] # XXX this is fragile
+        else: # one firstcal file for many data files
+            if isLinPol(pp):
+                filedict[f] = [firstcal_files[pp][0]]
+            else:
+                filedict[f] = [firstcal_files[lpk][0] for lpk in linear_pol_keys]
+
+jonesLookup = {
+    -5: (-5,-5),
+    -6: (-6,-6),
+    -7: (-5,-6),
+    -8: (-6,-5)
+}
 
 for f in args:
     mir = pyuvdata.UVData()
@@ -42,10 +72,11 @@ for f in args:
     cal = pyuvdata.UVCal()
     print "  Reading calibration : {0}".format(filedict[f])
     cal.read_calfits(filedict[f])
-
+    
     print "  Calibrating..."
     antenna_index = dict(zip(*(cal.antenna_numbers,range(cal.Nants_data))))
     for p,pol in enumerate(mir.polarization_array):
+        p1,p2 = [list(cal.jones_array).index(pk) for pk in jonesLookup[pol]] #XXX could replace with numpy function instead of casting to list
         for bl,k in zip(*np.unique(mir.baseline_array, return_index=True)):
             blmask = np.where(mir.baseline_array == bl)[0]
             ai, aj = mir.baseline_to_antnums(bl)
@@ -55,26 +86,26 @@ for f in args:
                 if cal.cal_type == 'gain' and cal.gain_convention == 'multiply':
                     mir.data_array[blmask, nsp, :, p] = \
                                 mir.data_array[blmask, nsp, :, p] * \
-                                cal.gain_array[antenna_index[ai], nsp, :, :, p].T * \
-                                np.conj(cal.gain_array[antenna_index[aj], nsp, :, :, p].T)
+                                cal.gain_array[antenna_index[ai], nsp, :, :, p1].T * \
+                                np.conj(cal.gain_array[antenna_index[aj], nsp, :, :, p2].T)
                                                                
                 if cal.cal_type == 'gain' and cal.gain_convention == 'divide':
                     mir.data_array[blmask, nsp, :, p] =  \
                                 mir.data_array[blmask, nsp, :, p] / \
-                                cal.gain_array[antenna_index[ai], nsp, :, :, p].T / \
-                                np.conj(cal.gain_array[antenna_index[aj], nsp, :, :, p].T)
+                                cal.gain_array[antenna_index[ai], nsp, :, :, p1].T / \
+                                np.conj(cal.gain_array[antenna_index[aj], nsp, :, :, p2].T)
     
                 if cal.cal_type == 'delay' and cal.gain_convention == 'multiply':
                     mir.data_array[blmask, nsp, :, p] =  \
                                 mir.data_array[blmask, nsp, :, p] * \
-                                np.exp(-2j*np.pi*np.dot(cal.delay_array[antenna_index[ai], nsp, :, p].reshape(-1,1),cal.freq_array)) * \
-                                np.conj(np.exp(-2j*np.pi*np.dot(cal.delay_array[antenna_index[aj], nsp, :, p].reshape(-1,1),cal.freq_array)))
+                                np.exp(-2j*np.pi*np.dot(cal.delay_array[antenna_index[ai], nsp, :, p1].reshape(-1,1),cal.freq_array)) * \
+                                np.conj(np.exp(-2j*np.pi*np.dot(cal.delay_array[antenna_index[aj], nsp, :, p2].reshape(-1,1),cal.freq_array)))
 
                 if cal.cal_type == 'delay' and cal.gain_convention == 'divide':
                     mir.data_array[blmask, nsp, :, p] =  \
                                 mir.data_array[blmask, nsp, :, p] / \
-                                np.exp(-2j*np.pi*np.dot(cal.delay_array[antenna_index[ai], nsp, :, p].reshape(-1,1),cal.freq_array)) / \
-                                np.conj(np.exp(-2j*np.pi*np.dot(cal.delay_array[antenna_index[aj], nsp, :, p].reshape(-1,1),cal.freq_array)))
+                                np.exp(-2j*np.pi*np.dot(cal.delay_array[antenna_index[ai], nsp, :, p1].reshape(-1,1),cal.freq_array)) / \
+                                np.conj(np.exp(-2j*np.pi*np.dot(cal.delay_array[antenna_index[aj], nsp, :, p2].reshape(-1,1),cal.freq_array)))
 
 
 
