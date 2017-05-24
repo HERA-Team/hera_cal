25f4e3f979a8dce53aa112099de54f3f20cecae8
Paul La Plante <plaplant@sas.upenn.edu>
2017-05-24 15:11:35 -0700

    Add batch script for making .uvcOR files; update other scripts

--- scripts/omni_apply.py
+++ scripts/omni_apply.py
@@ -5,7 +5,7 @@ import optparse, sys, pyuvdata, heracal, glob
 
 ### Options ###
 o = optparse.OptionParser()
-o.set_usage('omni_apply.py [options] *uvcRRE')
+o.set_usage('omni_apply.py [options] *uvc')
 o.set_description(__doc__)
 a.scripting.add_standard_options(o,pol=True)
 o.add_option('--omnipath',dest='omnipath',default='*.fits',type='string',
@@ -14,6 +14,8 @@ o.add_option('--median', action='store_true',
             help='Take the median in time before applying solution. Applicable only in delay.')
 o.add_option('--firstcal', action='store_true', 
             help='Applying firstcal solutions.')
+o.add_option('--extension',dest='extension',default='O',type='string',
+             help='Filename extension to be appended to the input filename')
 opts,args = o.parse_args(sys.argv[1:])
 args = np.sort(args)
 pols = opts.pol.split(',')
@@ -137,6 +139,6 @@ for f in args:
         print " Writing {0}".format(f+'F')
         mir.write_miriad(f +'F') 
     else:
-        print " Writing {0}".format(f+'O')
-        mir.write_miriad(f +'O') 
+        print " Writing {0}".format(f + opts.extension)
+        mir.write_miriad(f + opts.extension) 
 
