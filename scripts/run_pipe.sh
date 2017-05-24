6438ac28d798c998efc0b36bf9e98c34e24eb113
Zaki Ali <zakiali88@gmail.com>
2017-05-24 17:08:39 -0400

    modify run_pipe script

--- scripts/run_pipe.sh
+++ scripts/run_pipe.sh
@@ -5,14 +5,26 @@ cd ${jd}
 echo mkdir grid_output
 mkdir grid_output
 
+EX_ANTS_X=81
+EX_ANTS_Y=81
+
+# Find the badants. The EX_ANTS gets passed into batch_get_bad_ants
+echo badants_yid=`qsub -t 1-72 ~/src/heracal/scripts/batch_get_bad_ants.sh -a ${EX_ANTS_Y} /data4/paper/HERA2015/${jid}/zen.${jd}.*.yy.HH.uvc`
+badants_yid=`qsub -t 1-72 ~/src/heracal/scripts/batch_get_bad_ants.sh -a ${EX_ANTS_Y} /data4/paper/HERA2015/${jid}/zen.${jd}.*.yy.HH.uvc`
+badants_yid=`echo ${firstcal_yid} | cut -f 1 -d . | cut -f 3 -d ' '`
+
+echo badants_xid=`qsub -t 1-72 ~/src/heracal/scripts/batch_get_bad_ants.sh -a ${EX_ANTS_X} /data4/paper/HERA2015/${jid}/zen.${jd}.*.xx.HH.uvc`
+badants_xid=`qsub -t 1-72 ~/src/heracal/scripts/batch_get_bad_ants.sh -a ${EX_ANTS_X} /data4/paper/HERA2015/${jid}/zen.${jd}.*.xx.HH.uvc`
+badants_xid=`echo ${firstcal_yid} | cut -f 1 -d . | cut -f 3 -d ' '`
+
 #bad_ants in this script
-#echo firstcal_yid=`qsub -t 1-72 ~/src/mycapo/zsa/scripts/batch_firstcal.sh -p yy /data4/paper/HERA2015/${jid}/zen.${jd}.*.yy.HH.uvc`
-firstcal_yid=`qsub -t 1-72 ~/src/mycapo/zsa/scripts/batch_firstcal.sh -p yy  /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc`
+echo firstcal_yid=`qsub -hold_jid ${badants_yid} -t 1-72 ~/src/heracal/scripts/batch_firstcal.sh -p yy /data4/paper/HERA2015/${jid}/zen.${jd}.*.yy.HH.uvc`
+firstcal_yid=`qsub -hold_jid ${badants_yid} -t 1-72 ~/src/heracal/scripts/batch_firstcal.sh -p yy  /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc`
 firstcal_yid=`echo ${firstcal_yid} | cut -f 1 -d . | cut -f 3 -d ' '`
 
 #bad_ants in this script
-#echo firstcal_xid=`qsub -t 1-72 ~/src/mycapo/zsa/scripts/batch_firstcal.sh -p xx  /data4/paper/HERA2015/${jid}/zen.${jd}.*.xx.HH.uvc`
-firstcal_xid=`qsub -t 1-72 ~/src/mycapo/zsa/scripts/batch_firstcal.sh -p xx  /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc`
+echo firstcal_xid=`qsub -hold_jid ${badants_xid} -t 1-72 ~/src/heracal/scripts/batch_firstcal.sh -p xx  /data4/paper/HERA2015/${jid}/zen.${jd}.*.xx.HH.uvc`
+firstcal_xid=`qsub -hold_jid ${badants_xid} -t 1-72 ~/src/heracal/scripts/batch_firstcal.sh -p xx  /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc`
 firstcal_xid=`echo ${firstcal_xid} | cut -f 1 -d . | cut -f 3 -d ' '`
 
 
@@ -34,38 +46,37 @@ firstcal_xid=`echo ${firstcal_xid} | cut -f 1 -d . | cut -f 3 -d ' '`
 #firstcal_apply_xid=`echo ${firstcal_apply_xid} | cut -f 1 -d . | cut -f 3 -d ' '`
 
 
-echo firstcal_apply_yid=qsub -hold_jid ${firstcal_yid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_firstcal_apply.sh -p yy  /data4/paper/HERA2015/{jd}/zen.${jd}.*.yy.HH.uvc
-firstcal_apply_yid=`qsub -hold_jid ${firstcal_yid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_firstcal_apply_yy.sh -p yy  /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc`
-firstcal_apply_yid=`echo ${firstcal_apply_yid} | cut -f 1 -d . | cut -f 3 -d ' '`
-
-echo firstcal_apply_xid=qsub -hold_jid ${firstcal_xid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_firstcal_apply.sh -p xx  /data4/paper/HERA2015/{jd}/zen.${jd}.*.xx.HH.uvc
-firstcal_apply_xid=`qsub -hold_jid ${firstcal_xid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_firstcal_apply.sh -p xx  /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc`
-firstcal_apply_xid=`echo ${firstcal_apply_xid} | cut -f 1 -d . | cut -f 3 -d ' '`
-
+#echo firstcal_apply_yid=qsub -hold_jid ${firstcal_yid} -t 1-72 ~/src/heracal/scripts/batch_firstcal_apply.sh -p yy  /data4/paper/HERA2015/{jd}/zen.${jd}.*.yy.HH.uvc
+#firstcal_apply_yid=`qsub -hold_jid ${firstcal_yid} -t 1-72 ~/src/heracal/scripts/batch_firstcal_apply_yy.sh -p yy  /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc`
+#firstcal_apply_yid=`echo ${firstcal_apply_yid} | cut -f 1 -d . | cut -f 3 -d ' '`
+#
+#echo firstcal_apply_xid=qsub -hold_jid ${firstcal_xid} -t 1-72 ~/src/heracal/scripts/batch_firstcal_apply.sh -p xx  /data4/paper/HERA2015/{jd}/zen.${jd}.*.xx.HH.uvc
+#firstcal_apply_xid=`qsub -hold_jid ${firstcal_xid} -t 1-72 ~/src/heracal/scripts/batch_firstcal_apply.sh -p xx  /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc`
+#firstcal_apply_xid=`echo ${firstcal_apply_xid} | cut -f 1 -d . | cut -f 3 -d ' '`
 
 
 #bad_ants in this script
-echo omni_run_yid=qsub -hold_jid ${firstcal_apply_yid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_omni_run.sh -p yy  /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc
-omni_run_yid=`qsub -hold_jid ${firstcal_apply_yid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_omni_run.sh -p yy  /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc`
+echo omni_run_yid=qsub -hold_jid ${firstcal_yid} -t 1-72 ~/src/heracal/scripts/batch_omni_run.sh -p yy  /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc
+omni_run_yid=`qsub -hold_jid ${firstcal_yid} -t 1-72 ~/src/heracal/scripts/batch_omni_run.sh -p yy  /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc`
 omni_run_yid=`echo ${omni_run_yid} | cut -f 1 -d . | cut -f 3 -d ' '`
 
 #bad_ants in this script
-echo omni_run_xid=qsub -hold_jid ${firstcal_apply_xid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_omni_run.sh -p xx  /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc
-omni_run_xid=`qsub -hold_jid ${firstcal_apply_xid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_omni_run.sh -p xx  /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc`
+echo omni_run_xid=qsub -hold_jid ${firstcal_xid} -t 1-72 ~/src/heracal/scripts/batch_omni_run.sh -p xx  /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc
+omni_run_xid=`qsub -hold_jid ${firstcal_xid} -t 1-72 ~/src/heracal/scripts/batch_omni_run.sh -p xx  /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc`
 omni_run_xid=`echo ${omni_run_xid} | cut -f 1 -d . | cut -f 3 -d ' '`
 
 
-echo omni_apply_yid=qsub -hold_jid ${omni_run_yid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_omni_apply.sh -p yy  /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc
-omni_apply_yid=`qsub -hold_jid ${omni_run_yid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_omni_apply.sh -p yy  /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc`
-echo omni_apply_xid=qsub -hold_jid ${omni_run_xid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_omni_apply.sh -p xx  /data4/paper/HERA2015/${jd}/zen.{jd}.*.xx.HH.uvc
-omni_apply_xid=`qsub -hold_jid ${omni_run_xid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_omni_apply.sh -p xx  /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc`
+echo omni_apply_yid=qsub -hold_jid ${omni_run_yid} -t 1-72 ~/src/heracal/scripts/batch_omni_apply.sh -p yy  /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc
+omni_apply_yid=`qsub -hold_jid ${omni_run_yid} -t 1-72 ~/src/heracal/scripts/batch_omni_apply.sh -p yy  /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc`
+echo omni_apply_xid=qsub -hold_jid ${omni_run_xid} -t 1-72 ~/src/heracal/scripts/batch_omni_apply.sh -p xx  /data4/paper/HERA2015/${jd}/zen.{jd}.*.xx.HH.uvc
+omni_apply_xid=`qsub -hold_jid ${omni_run_xid} -t 1-72 ~/src/heracal/scripts/batch_omni_apply.sh -p xx  /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc`
 
 
 # run xrfi
-echo xrfi_yid=qsub  -hold_jid ${omni_run_yid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_xrfi.sh /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc.omni.calfits
-xrfi_yid=`qsub  -hold_jid ${omni_run_yid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_xrfi.sh /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc.omni.calfits`
-echo xrfi_xid=qsub  -hold_jid ${omni_run_xid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_xrfi.sh /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc.omni.calfits
-xrfi_xid=`qsub  -hold_jid ${omni_run_xid} -t 1-72 ~/src/mycapo/zsa/scripts/batch_xrfi.sh /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc.omni.calfits`
+echo xrfi_yid=qsub  -hold_jid ${omni_run_yid} -t 1-72 ~/src/heracal/scripts/batch_xrfi.sh /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc.omni.calfits
+xrfi_yid=`qsub  -hold_jid ${omni_run_yid} -t 1-72 ~/src/heracal/scripts/batch_xrfi.sh /data4/paper/HERA2015/${jd}/zen.${jd}.*.yy.HH.uvc.omni.calfits`
+echo xrfi_xid=qsub  -hold_jid ${omni_run_xid} -t 1-72 ~/src/heracal/scripts/batch_xrfi.sh /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc.omni.calfits
+xrfi_xid=`qsub  -hold_jid ${omni_run_xid} -t 1-72 ~/src/heracal/scripts/batch_xrfi.sh /data4/paper/HERA2015/${jd}/zen.${jd}.*.xx.HH.uvc.omni.calfits`
 
 echo cd ..
 cd ..
