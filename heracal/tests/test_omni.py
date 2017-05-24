b56a639dbc5b5a45a4829d1f62a448edbb5a8610
Zaki Ali <zakiali88@gmail.com>
2017-05-23 23:50:19 -0400

    update UVData_to_dict function to handle filenames.
    add test for it

--- heracal/tests/test_omni.py
+++ heracal/tests/test_omni.py
@@ -404,8 +404,9 @@ class TestMethods(object):
 
     def test_UVData_to_dict(self):
         str2pol = {'xx':-5, 'yy': -6, 'xy':-7, 'yy':-8}
+        filename = os.path.join(DATA_PATH,'zen.2457698.40355.xx.HH.uvcAA')
         uvd =  UVData()
-        uvd.read_miriad(os.path.join(DATA_PATH,'zen.2457698.40355.xx.HH.uvcAA'))
+        uvd.read_miriad(filename)
         if uvd.phase_type != 'drift':
             uvd.unphase_to_drift()
     
@@ -417,6 +418,13 @@ class TestMethods(object):
                 np.testing.assert_equal(d[i,j][pol], np.resize(uvd.data_array[uvmask][:,0,:,uvpol], d[i,j][pol].shape))
                 np.testing.assert_equal(f[i,j][pol], np.resize(uvd.flag_array[uvmask][:,0,:,uvpol], f[i,j][pol].shape))
         
+        d,f = omni.UVData_to_dict([filename, filename])
+        for i,j in d:
+            for pol in d[i,j]:
+                uvpol = list(uvd.polarization_array).index(str2pol[pol])
+                uvmask = np.all(np.array(zip(uvd.ant_1_array, uvd.ant_2_array)) == [i,j], axis=1)
+                np.testing.assert_equal(d[i,j][pol], np.resize(uvd.data_array[uvmask][:,0,:,uvpol], d[i,j][pol].shape))
+                np.testing.assert_equal(f[i,j][pol], np.resize(uvd.flag_array[uvmask][:,0,:,uvpol], f[i,j][pol].shape))
         
         
         
