a3d7adc0e6f861bbb903fad8db32523621260206
Saul A. Kohn <saul.aryeh.kohn@gmail.com>
2017-06-11 14:43:22 -0400

    use omni.getOptionParser within run and apply scripts

--- scripts/omni_apply.py
+++ scripts/omni_apply.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python
 import numpy as np
 import optparse
-from heracal.omni import omni_apply
-import aipy, sys
+from heracal import omni
+import sys
 
 ### Options ###
-o = optparse.OptionParser()
-o.set_usage('omni_apply.py [options] *uvc')
-o.set_description(__doc__)
-aipy.scripting.add_standard_options(o, pol=True)
-o.add_option('--omnipath', dest='omnipath', default='*.fits', type='string',
-             help='Filename or format string that gets passed to glob for omnical/firstcal solution fits files.')
-o.add_option('--median', action='store_true',
-             help='Take the median in time before applying solution. Applicable only in delay.')
-o.add_option('--firstcal', action='store_true',
-             help='Applying firstcal solutions.')
-o.add_option('--extension', dest='extension', default='O', type='string',
-             help='Filename extension to be appended to the input filename')
+o = omni.get_optionParser('omni_apply')
 opts, args = o.parse_args(sys.argv[1:])
 args = np.sort(args)
 
-omni_apply(args, opts)
+omni.omni_apply(args, opts)
--- scripts/omni_run.py
+++ scripts/omni_run.py
@@ -1,30 +1,11 @@
 #! /usr/bin/env python
 
-import aipy
-import numpy as np
-from heracal.omni import omni_run
-import pyuvdata
+from heracal import omni
 import optparse
-import os
 import sys
 
-o = optparse.OptionParser()
-o.set_usage(
-    "omni_run.py -C [calfile] -p [pol] --firstcal=[firstcal path] [options] *.uvc")
-o.set_description(__doc__)
-aipy.scripting.add_standard_options(o, cal=True, pol=True)
-o.add_option('--omnipath', dest='omnipath', default='.', type='string',
-             help='Path to save omnical solutions.')
-o.add_option('--ex_ants', dest='ex_ants', default=None,
-             help='Antennas to exclude, separated by commas.')
-o.add_option('--firstcal', dest='firstcal', type='string',
-             help='Path and name of firstcal file. Can pass in wildcards.')
-o.add_option('--minV', action='store_true',
-             help='Toggle V minimization capability. This only makes sense in the case of 4-pol cal, which will set crosspols (xy & yx) equal to each other')
-o.add_option('--median', action='store_true',
-             help='Take the median over time of the starting calibration gains (e.g. firstcal).')
+o = omni.get_optionParser('omni_run')
 opts, files = o.parse_args(sys.argv[1:])
-
 history = ' '.join(sys.argv)
 
-omni_run(files, opts, history)
+omni.omni_run(files, opts, history)
