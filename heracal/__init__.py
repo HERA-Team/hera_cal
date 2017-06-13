b1e8b18403eeb3ced996d219f9d704ef69f7bf58
Paul La Plante <plaplant@sas.upenn.edu>
2017-06-13 10:56:11 -0700

    Add coveralls rc file; update init file

--- heracal/__init__.py
+++ heracal/__init__.py
@@ -3,6 +3,3 @@ import firstcal
 import xrfi
 import metrics
 import os
-# import data
-# CAL_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../calibrations/')
-# DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '/data/')
