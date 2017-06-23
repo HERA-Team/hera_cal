dc71d8dd717f7e34ee3b29812969d07801fa7e94
Saul A. Kohn <saul.aryeh.kohn@gmail.com>
2017-06-23 11:54:18 -0400

    setup.py script change again

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ from os import listdir
 __version__ = '0.0.0'
 
 setup_args = {
-    'name': 'heracal',
+    'name': 'hera_cal',
     'author': 'HERA Team',
-    'url': 'https://github.com/HERA-Team/heracal',
+    'url': 'https://github.com/HERA-Team/hera_cal',
     'license': 'BSD',
     'description': 'collection of calibration routines to run on the HERA instrument.',
-    'package_dir': {'heracal': 'heracal'},
-    'packages': ['heracal'],
+    'package_dir': {'hera_cal': 'hera_cal'},
+    'packages': ['hera_cal'],
     #    'scripts': glob.glob('scripts/*'),
     'version': __version__,
-    'package_data': {'heracal': ['data/*py', 'data/*uv*/*', 'data/test_input/*', 'calibrations/*']},
+    'package_data': {'hera_cal': ['data/*py', 'data/*uv*/*', 'data/test_input/*', 'calibrations/*']},
     #    'install_requires': ['numpy>=1.10', 'scipy', 'pyuvdata', 'astropy>1.2', 'aipy']
     #    'dependency_links': ['https://github.com/zakiali/omnical/tarball/master#egg=omnical-dev',]
     'zip_safe': False,
