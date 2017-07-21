76c2782c9bcb1ec5d31f47745d5f9ec2059619c4
Danny Jacobs <daniel.c.jacobs@asu.edu>
2017-07-21 11:20:52 -0700

    Test mc integration.

--- hera_cal/tests/test_firstcal.py
+++ hera_cal/tests/test_firstcal.py
@@ -312,3 +312,17 @@ class Test_firstcal_run(object):
         nt.assert_true(os.path.exists(objective_file))
         os.remove(objective_file)
         return
+    def test_mc_calfile(self):
+        objective_file = os.path.join(
+            DATA_PATH, 'zen.2457698.40355.xx.HH.uvcAA.first.calfits')
+        xx_vis4real = os.path.join(DATA_PATH, xx_vis)
+        if os.path.exists(objective_file):
+            os.remove(objective_file)
+        o = firstcal.firstcal_option_parser()
+        cmd = "-p xx --ex_ants=81 {1}".format(xx_vis4real)
+        opts, files = o.parse_args(cmd.split())
+        history = 'history'
+        firstcal.firstcal_run(files, opts, history)
+        nt.assert_true(os.path.exists(objective_file))
+        os.remove(objective_file)
+        return
