cef6dc9f2a97c2dc5bd582131cd670b8c02ae4c7
Danny Jacobs <daniel.c.jacobs@asu.edu>
2017-07-27 13:31:53 -0700

    Added test checks different numbers of antennas show up in different epochs.

--- hera_cal/tests/test_utils.py
+++ hera_cal/tests/test_utils.py
@@ -4,6 +4,12 @@ import numpy as np,sys
 from hera_cal.calibrations import CAL_PATH
 freqs = np.array([0.15])
 
+def count_ants(aa):
+    nants = 0
+    for antpos in aa.antpos_ideal:
+        if antpos[0]!=-1 and antpos[1] != -1 and antpos[2] != -1:
+            nants += 1
+    return nants
 class Test_utils(object):
     # add directory with calfile
     if CAL_PATH not in sys.path:
@@ -23,3 +29,8 @@ class Test_utils(object):
         #check the position is correct to ~6m
         nt.assert_almost_equal(aa.lat,-30.7215261207*np.pi/180,places=6)
         nt.assert_almost_equal(aa.lon,21.4283038269*np.pi/180,places=6)
+    def test_mc_nants(self):
+        aa = get_HERA_aa(freqs,array_epoch_jd=2457458)
+        nt.assert_equal(count_ants(aa),20)
+        aa = get_HERA_aa(freqs,array_epoch_jd=2457962)
+        nt.assert_equal(count_ants(aa),34)
