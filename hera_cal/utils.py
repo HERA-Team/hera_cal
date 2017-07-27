e0a47362ca946cb3078ba82933d9e0d2e3052302
Danny Jacobs <daniel.c.jacobs@asu.edu>
2017-07-27 13:06:26 -0700

    Decent docstring for get_HERA_aa and defer all defaults to cal file.

--- hera_cal/utils.py
+++ hera_cal/utils.py
@@ -1,28 +1,28 @@
 import os
-def get_HERA_aa(freqs,calfile='hera_cm',
-                locations_file=None,
-                array_epoch_jd=None):
-    # create an aipy AntennaArray object with the HERA configuration as it
-    # was on array_epoch_jd.
+def get_HERA_aa(freqs,calfile='hera_cm',**kwargs):
+    # create an aipy AntennaArray object using positions and hookup info in M&C.
     #
-    # A default file is supplied but will soon be out of date.
-    #   - create a new one with hera_mc/scripts/XXX.py
+    # Inputs:
+    # freqs = numpy array of frequencies in GHz
+    #   - used to compute uvws by some aipy functions
+    #   - for applications using antenna locations in meters, a nominal input
+    #        value of np.array([0.15]) is probably fine
+    # calfile = python library containing get_aa function.
+    #   - default value is heral_cal.hera_cm handles import from M&C
+    #   - don't include the .py
+    #   - needs to be in your python path
+    # array_epoch_id = julian date of desired configuration
+    #   - if not input uses default date set in calfile, hera_cal.hera_cm
+    # locations_file = antenna location csv file exported from m&c
+    #   - default file included
+    #   - create a new one with hera_mc/scripts/write_antenna_location_file.py
     #
-    # NOTE, if you didn't specify a array_epoch_jd, one will be chosen for you
-    # by the cal file.
     #
-    # Also supports loading of old style cal files, just set calfile away from default
     #
 
-    #sometimes the default option is None, but we really mean no input
+    #sometimes the default option is None, but we really mean default
     if calfile is None:
         calfile='hera_cm'
-        #set defaults that hera_cm needs.
-    if locations_file is None:
-        locations_file = os.path.join(os.path.dirname(__file__), 'data/hera_ant_locs_05_16_2017.csv')
-    if array_epoch_jd is None:
-        array_epoch_jd = 2457458  #need a date
-
 
     exec('from {calfile} import get_aa'.format(calfile=calfile))
     if calfile!='hera_cm': #load an alternate cal file
@@ -30,4 +30,4 @@ def get_HERA_aa(freqs,calfile='hera_cm',
         #IT IS PROBABLY NOT UP TO DATE
         return get_aa(freqs)
     else:
-        return get_aa(freqs,locations_file,array_epoch_jd)
+        return get_aa(freqs,**kwargs)
