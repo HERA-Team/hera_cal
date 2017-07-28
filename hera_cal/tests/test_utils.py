647385ec95ba8d958c2c2b9af13955119e3a91ed
Danny Jacobs <daniel.c.jacobs@asu.edu>
2017-07-27 17:43:00 -0700

    Basic test that the cal file round trips positions correctly.

--- hera_cal/tests/test_utils.py
+++ hera_cal/tests/test_utils.py
@@ -34,3 +34,20 @@ class Test_utils(object):
         nt.assert_equal(count_ants(aa),20)
         aa = get_HERA_aa(freqs,array_epoch_jd=2457962)
         nt.assert_equal(count_ants(aa),34)
+    def test_aa_get_params(self):
+        #this is a super rough check that we've round-tripped the topo coords
+        #   through equatorial nanoseconds without massively missing on units
+        # just tests that we get to within 10%.
+        aa = get_HERA_aa(freqs,array_epoch_jd=2457458)
+        prms = aa.get_params({'9':['x','top_x','top_y','top_z']}) #get the position of antenna 8
+        nt.assert_equal(prms['9']['x'],-143.64149291430934)
+        radius_of_earth = 6371e3
+        #use positions taken from hera_ant_locs_05_16_2017.csv
+        topo_x_rough = (21.4272063669-21.4283038269)*np.pi/180
+        topo_x_rough *= np.cos(-30.7215261207*np.pi/180)*radius_of_earth
+        topo_y_rough = (-30.7222964631 - -30.7215261207)*np.pi/180
+        topo_y_rough *= radius_of_earth
+        x_err = np.abs((topo_x_rough - prms['9']['top_x'])/prms['9']['top_x'])
+        y_err = np.abs((topo_y_rough - prms['9']['top_y'])/prms['9']['top_y'])
+        nt.assert_true(x_err<0.1)
+        nt.assert_true(y_err<0.1)
