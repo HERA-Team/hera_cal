b53fe4d603b23f61a8ac0cc91dc83a01c32abc3b
Adam Beardsley <adam.p.beardsley@gmail.com>
2017-09-01 16:25:47 +0200

    Adding option to use antenna positions to fix uvws

--- scripts/extract_hh.py
+++ scripts/extract_hh.py
@@ -3,6 +3,8 @@ import os
 import argparse
 import pyuvdata
 from hera_cal.version import version_info
+import pyuvdata.utils as uvutils
+import numpy as np
 
 parser = argparse.ArgumentParser(description='Extract HERA hex antennas from data '
                                  'file, and save with new extension.')
@@ -11,6 +13,8 @@ parser.add_argument('--extension', type=str, help='Extension to be appended to '
 parser.add_argument('--filetype', type=str, help='Input and output file type. '
                     'Allowed values are "miriad" (default), and "uvfits".',
                     default='miriad')
+parser.add_argument('--fixuvws', action='store_true', help='Optional flag to '
+                    'use antenna positions to replace uvws.')
 parser.add_argument('files', metavar='files', type=str, nargs='+',
                     help='Files to be processed.')
 args = parser.parse_args()
@@ -29,6 +33,13 @@ for filename in args.files:
     uv.select(antenna_nums=uv.antenna_numbers[ind])
     uv.history += ' Hera Hex antennas selected with hera_cal/scripts/extract_hh.py' \
                   ', hera_cal version: ' + str(version_info) + '.'
+    if args.fixuvws:
+        antpos = uv.antenna_positions + uv.telescope_location
+        antpos = uvutils.ENU_from_ECEF(antpos.T, *uv.telescope_location_lat_lon_alt).T
+        antmap = -np.ones(np.max(uv.antenna_numbers) + 1, dtype=int)
+        for i, ant in enumerate(uv.antenna_numbers):
+            antmap[ant] = i
+        uv.uvw_array = antpos[antmap[uv.ant_2_array], :] - antpos[antmap[uv.ant_1_array], :]
     if args.filetype is 'miriad':
         base, ext = os.path.splitext(filename)
         uv.write_miriad(base + '.' + args.extension + ext)
