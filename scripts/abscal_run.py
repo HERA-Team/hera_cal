949df84d4ac928f08a3162f17ddee27c71769317
Nicholas Kern <nkern@berkeley.edu>
2017-11-16 10:10:47 -0800

    added abscal_run script 	modified:   hera_cal/abscal.py 	new file:   scripts/abscal_run.py

---
+++ scripts/abscal_run.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+from hera_cal import abscal
+import sys
+import os
+
+a = abscal.abscal_arg_parser()
+args = a.parse_args()
+history = ' '.join(sys.argv)
+
+abscal.run_abscal(args.data_file, args.model_files,
+                  calfits_fname=args.calfits_fname,
+                  overwrite=args.overwrite,
+                  verbose=args.silence is False,
+                  unravel_time=args.unravel_time,
+                  save=True)
+
