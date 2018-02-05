7fbf5a47b08b04609fc3b3e14420a16071e2c38c
Nicholas Kern <nkern@berkeley.edu>
2018-02-05 02:22:23 -0800

    added test to data_to_miriad to ensure ant pos are properly set

--- hera_cal/tests/test_utils.py
+++ hera_cal/tests/test_utils.py
@@ -157,6 +157,12 @@ def test_data_to_miriad():
     d, fl, ap, a, f, t, l, p = abscal.UVData2AbsCalDict(fp, return_meta=True)
     # test basic execution
     utils.data_to_miriad("ex.uv", d, l, f, ap, flags=fl, outdir="./", start_jd=2458042)
+    # ensure antenna positions are correct
+    uvd = UVData()
+    uvd.read_miriad('ex.uv')
+    ap, a = uvd.get_ENU_antpos(center=False)
+    ap = dict(zip(a, ap))
+    nt.assert_almost_equal(sum(np.abs(np.array([1.46078420e+01,5.57881090e-02,-9.77543007e-03]) - (ap[25] - ap[24]))), 0, delta=1e-6)
     # test w/ no flags
     utils.data_to_miriad("ex.uv", d, l, f, ap, outdir="./", start_jd=2458042, overwrite=True)
     nt.assert_true(os.path.exists('ex.uv'))
@@ -171,5 +177,9 @@ def test_data_to_miriad():
     uvd = utils.data_to_miriad("ex.uv", d, l, f, ap, outdir="./", start_jd=2458042, overwrite=True, return_uvdata=True)
     nt.assert_true(isinstance(uvd, UVData))
     shutil.rmtree('ex.uv')
+
+
+
+
 
 
