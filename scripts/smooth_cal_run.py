818f7e2b32c4880637c8e7429315de5befe37547
Josh Dillon <jsdiillon@berkeley.edu>
2018-03-08 14:33:14 -0800

    improved script documentation

--- scripts/smooth_cal_run.py
+++ scripts/smooth_cal_run.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python2.7
-"""Command-line drive script for hera_cal.smooth_cal"""
+
+"""Command-line drive script for hera_cal.smooth_cal. By default, this script
+1) loads in the calibration solutions and associated data
+2) performs a time smoothing of the calibration solutions (e.g. averaging) using Gaussian kernel (default 120 s FWHM)
+3) performs a frequency smoothing by delay-filtering the solutions (default 10 MHz scale e.g. 100 ns delay filter).
+4) writes the smoothed calibrations solutions to disk.
+Frequency smoothing is generally more important and the two smoothing operations do not commute as currently written,
+so frequency smoothing is performed second to ensure frequency smoothness of calibration solutions. To ensure that 
+time smoothing does not introduce file boundary discontinuities, one can provide previous and subsequent calibration
+and data files that are used when time smoothing but are not themselves modified by this code. See help for a more
+detailed explanation of the parameters.
+"""
+
 from hera_cal.smooth_cal import Calibration_Smoother, smooth_cal_argparser
 import sys
 
