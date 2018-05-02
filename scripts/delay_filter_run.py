31cadcb4d97786bd0e65ec8dd854ef5f8d3608ac
Nicholas Kern <nkern@berkeley.edu>
2018-05-02 01:16:53 -0700

    added min_dly and gain kwargs to delay filter

--- scripts/delay_filter_run.py
+++ scripts/delay_filter_run.py
@@ -22,7 +22,7 @@ df = Delay_Filter()
 df.load_data(a.infile, filetype=a.filetype)
 df.run_filter(standoff=a.standoff, horizon=a.horizon, tol=a.tol, window=a.window,
               skip_wgt=a.skip_wgt, maxiter=a.maxiter, flag_nchan_low=a.flag_nchan_low,
-              flag_nchan_high=a.flag_nchan_high, **kwargs)
+              flag_nchan_high=a.flag_nchan_high, min_dly=a.min_dly, gain=a.gain, **kwargs)
 
 # Write high-pass residual
 if a.write_model == False or a.write_all == True:
