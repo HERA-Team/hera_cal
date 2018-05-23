e5e865094449c839e9f8b8e404b2945e68bacaa0
Nicholas Kern <nkern@berkeley.edu>
2018-05-22 22:01:58 -0700

    modified:   apply_cal.py 	modified:   ../scripts/apply_cal.py

--- scripts/apply_cal.py
+++ scripts/apply_cal.py
@@ -6,7 +6,12 @@ import sys
 
 a = ac.apply_cal_argparser()
 args = a.parse_args()
+
+kwargs = {}
+if args.vis_units is not None:
+    kwargs['vis_units'] = args.vis_units
+
 ac.apply_cal(args.infile, args.outfile, args.new_cal, old_calibration=args.old_cal, flags_npz=args.flags_npz, 
              flag_nchan_low=args.flag_nchan_low, flag_nchan_high=args.flag_nchan_high, filetype=args.filetype, 
              gain_convention=args.gain_convention, add_to_history = ' '.join(sys.argv), clobber=args.clobber,
-             vis_units=args.vis_units)
+             **kwargs)
