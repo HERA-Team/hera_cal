85ccd9e4e5eba2314aced9b87b86014bdf01f0f1
Nicholas Kern <nkern@berkeley.edu>
2018-06-28 14:40:20 +0800

    added fixed_start to lstbin 	modified:   ../lstbin.py 	modified:   test_lstbin.py

--- hera_cal/lstbin.py
+++ hera_cal/lstbin.py
@@ -499,7 +499,8 @@ def lst_bin_arg_parser():
     return a
 
 
-def config_lst_bin_files(data_files, dlst=None, atol=1e-10, lst_start=0.0, verbose=True, ntimes_per_file=60):
+def config_lst_bin_files(data_files, dlst=None, atol=1e-10, lst_start=0.0, fixed_start=False, verbose=True, 
+                         ntimes_per_file=60):
     """
     Configure lst grid, starting LST and output files given input data files and LSTbin params.
 
@@ -512,6 +513,9 @@ def config_lst_bin_files(data_files, dlst=None, atol=1e-10, lst_start=0.0, verbo
     dlst : type=float, LST bin width. If None, will get this from the first file in data_files.
 
     lst_start : type=float, starting LST for binner as it sweeps from lst_start to lst_start + 2pi.
+
+    fixed_start : type=bool, if True, LST grid starts at lst_start, regardless of LST of first data
+        record. Otherwise, LST grid starts at LST of first data record.
 
     ntimes_per_file : type=int, number of LST bins in a single output file
 
@@ -559,6 +563,8 @@ def config_lst_bin_files(data_files, dlst=None, atol=1e-10, lst_start=0.0, verbo
             dt -= 2 * np.pi
 
     # create lst_grid
+    if fixed_start:
+        start_lst = lst_start
     lst_grid = make_lst_grid(dlst, lst_start=start_lst, verbose=verbose)
     dlst = np.median(np.diff(lst_grid))
 
@@ -580,8 +586,8 @@ def config_lst_bin_files(data_files, dlst=None, atol=1e-10, lst_start=0.0, verbo
 
 
 def lst_bin_files(data_files, dlst=None, verbose=True, ntimes_per_file=60, file_ext="{}.{}.{:7.5f}.uv",
-                  outdir=None, overwrite=False, history=' ', lst_start=0, atol=1e-6, sig_clip=True,
-                  sigma=5.0, min_N=5, rephase=False, output_file_select=None, **kwargs):
+                  outdir=None, overwrite=False, history=' ', lst_start=0, fixed_start=False, atol=1e-6, 
+                  sig_clip=True, sigma=5.0, min_N=5, rephase=False, output_file_select=None, **kwargs):
     """
     LST bin a series of miriad files with identical frequency bins, but varying
     time bins. Output miriad file meta data (frequency bins, antennas positions, time_array)
@@ -596,6 +602,9 @@ def lst_bin_files(data_files, dlst=None, verbose=True, ntimes_per_file=60, file_
     dlst : type=float, LST bin width. If None, will get this from the first file in data_files.
 
     lst_start : type=float, starting LST for binner as it sweeps from lst_start to lst_start + 2pi.
+
+    fixed_start : type=bool, if True, LST grid starts at lst_start, regardless of LST of first data
+        record. Otherwise, LST grid starts at LST of first data record.
 
     ntimes_per_file : type=int, number of LST bins in a single output file
 
@@ -624,7 +633,7 @@ def lst_bin_files(data_files, dlst=None, verbose=True, ntimes_per_file=60, file_
     zen.{pol}.STD.{file_lst}.uv : holds LST bin stand dev along real and imag (data_array)
     """
     (data_times, lst_grid, dlst, file_lsts,
-     start_lst) = config_lst_bin_files(data_files, dlst=dlst, lst_start=lst_start,
+     start_lst) = config_lst_bin_files(data_files, dlst=dlst, lst_start=lst_start, fixed_start=fixed_start,
                                        ntimes_per_file=ntimes_per_file, verbose=verbose)
     nfiles = len(file_lsts)
 
