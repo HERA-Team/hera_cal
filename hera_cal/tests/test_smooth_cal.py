4d28d6cb7dcb1cfd132be6042ae777d653a71520
Josh Dillon <jsdillon@berkeley.edu>
2018-08-31 14:15:23 -0700

    python3 stuff

--- hera_cal/tests/test_smooth_cal.py
+++ hera_cal/tests/test_smooth_cal.py
@@ -2,6 +2,7 @@
 # Copyright 2018 the HERA Project
 # Licensed under the MIT License
 
+from __future__ import absolute_import, division, print_function
 from hera_cal import io
 from hera_cal import smooth_cal
 from hera_cal.datacontainer import DataContainer
