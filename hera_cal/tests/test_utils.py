6719062539718e318a25590833c7bc9a39854e36
Josh Dillon <jsdillon@berkeley.edu>
2018-10-02 13:09:40 -0700

    add unit test for noise variance prediction

--- hera_cal/tests/test_utils.py
+++ hera_cal/tests/test_utils.py
@@ -18,6 +18,7 @@ import glob
 from collections import OrderedDict as odict
 import copy
 import hera_cal as hc
+import scipy
 
 
 class Test_Pol_Ops(object):
@@ -484,3 +485,18 @@ def test_chisq():
     nt.assert_true(len(nObs) == 0)
     nt.assert_true(len(chisq_per_ant) == 0)
     nt.assert_true(len(chisq_per_ant) == 0)
+
+
+def test_predict_noise_variance_from_autos():
+    hd = io.HERAData(os.path.join(DATA_PATH, 'zen.2458098.43124.subband.uvh5'))
+    data, flags, nsamples = hd.read(polarizations=['xx', 'yy'])
+    for k in data.keys():
+        if k[0] != k[1]:
+            sigmasq = utils.predict_noise_variance_from_autos(k, data)
+            kernel = [[1,-2,1],[-2,4,-2],[1,-2,1]]
+            sigma = scipy.signal.convolve2d(data[k], kernel, mode='same', boundary='wrap')
+            sigma /= np.sum(np.array(kernel)**2)**.5
+            np.testing.assert_array_equal(np.abs(np.mean(np.mean(np.abs(sigma)**2, axis=0) / np.mean(sigmasq, axis=0)) - 1) <= .1, True)
+            times = hd.times_by_bl[k[:2]]
+            sigmasq2 = utils.predict_noise_variance_from_autos(k, data, df=(hd.freqs[1] - hd.freqs[0]), dt=((times[1] - times[0]) * 24. * 3600.))
+            np.testing.assert_array_equal(sigmasq, sigmasq2)
