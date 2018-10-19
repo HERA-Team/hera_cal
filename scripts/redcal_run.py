2b43e32ca1b7263d9d45c7c52b39380408671744
Josh Dillon <jsdillon@berkeley.edu>
2018-10-19 13:03:39 -0700

    Add max_rerun option

--- scripts/redcal_run.py
+++ scripts/redcal_run.py
@@ -15,5 +15,5 @@ a = redcal_argparser()
 
 redcal_run(a.input_data, firstcal_suffix=a.firstcal_suffix, omnical_suffix=a.omnical_suffix, omnivis_suffix=a.omnivis_suffix, 
            outdir=a.outdir, ant_metrics_file=a.ant_metrics_file, clobber=a.clobber, nInt_to_load=a.nInt_to_load, pol_mode=a.pol_mode, 
-           ex_ants=a.ex_ants, ant_z_thresh=a.ant_z_thresh, solar_horizon=a.conv_crit, conv_crit=a.conv_crit, maxiter=a.maxiter, 
+           ex_ants=a.ex_ants, ant_z_thresh=a.ant_z_thresh, max_rerun=a.max_rerun, solar_horizon=a.conv_crit, conv_crit=a.conv_crit, maxiter=a.maxiter, 
            check_every=a.check_every, check_after=a.check_after, gain=a.gain, append_to_history=' '.join(sys.argv), verbose=a.verbose)
