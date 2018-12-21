701078029c7ef6ae7d5ed72eb2e07ba89b9419cd
Josh Dillon <jsdillon@berkeley.edu>
2018-12-21 14:52:02 -0800

    add unit tests for interleaved noise variance estimation
    also updated test_predict_noise_variance_from_autos to use it

--- hera_cal/tests/test_noise.py
+++ hera_cal/tests/test_noise.py
@@ -10,7 +10,7 @@ import unittest
 import numpy as np
 import os
 import sys
-import scipy
+
 
 from hera_cal import io
 from hera_cal import noise
@@ -21,16 +21,40 @@ from hera_cal.apply_cal import apply_cal
 
 class Test_Noise(unittest.TestCase):
 
+    def test_interleaved_noise_variance_estimate(self):
+        const_test = noise.interleaved_noise_variance_estimate(np.ones((10, 10)))
+        np.testing.assert_array_equal(const_test, 0)
+
+        np.random.seed(21)
+        gauss_test = np.mean(noise.interleaved_noise_variance_estimate(np.random.randn(1000, 1000)))
+        np.testing.assert_almost_equal(gauss_test, 1, decimal=2)
+
+        kernels = [('2x2 diff', [[1,-1],[-1,1]]),
+                   ('2D plus', [[0,1,0],[1,-4,1],[0,1,0]]),
+                   ('2D box', [[1,1,1],[1,-8,1],[1,1,1]]),
+                   ('2D hybrid', [[1,-2,1],[-2,4,-2],[1,-2,1]]),
+                   ('1D 2-term', [[1,-1]]),
+                   ('1D 3-term', [[1,-2,1]]),
+                   ('1D 5-term', [[-1,4,-6,4,-1]]),
+                   ('1D 7-term', [[2,-9,18, -22, 18, -9, 2]])]
+        for kname, kernel in kernels:
+            gauss_test = np.mean(noise.interleaved_noise_variance_estimate(np.random.randn(1000, 1000), kernel=kernel))
+            np.testing.assert_almost_equal(gauss_test, 1, decimal=2)
+
+        with self.assertRaises(AssertionError):
+            noise.interleaved_noise_variance_estimate(np.random.randn(10,10), kernel=[[.5, 1.0, .5]])
+        with self.assertRaises(ValueError):
+            noise.interleaved_noise_variance_estimate(np.random.randn(10,10), kernel=[-1, 1])
+
+
     def test_predict_noise_variance_from_autos(self):
         hd = io.HERAData(os.path.join(DATA_PATH, 'zen.2458098.43124.subband.uvh5'))
         data, flags, nsamples = hd.read()
         for k in data.keys():
             if k[0] != k[1]:
                 sigmasq = noise.predict_noise_variance_from_autos(k, data)
-                kernel = [[1, -2, 1], [-2, 4, -2], [1, -2, 1]]
-                sigma = scipy.signal.convolve2d(data[k], kernel, mode='same', boundary='wrap')
-                sigma /= np.sum(np.array(kernel)**2)**.5
-                np.testing.assert_array_equal(np.abs(np.mean(np.mean(np.abs(sigma)**2, axis=0) / np.mean(sigmasq, axis=0)) - 1) <= .1, True)
+                noise_var = noise.interleaved_noise_variance_estimate(data[k])
+                np.testing.assert_array_equal(np.abs(np.mean(np.mean(noise_var, axis=0) / np.mean(sigmasq, axis=0)) - 1) <= .1, True)
                 times = hd.times_by_bl[k[:2]]
                 sigmasq2 = noise.predict_noise_variance_from_autos(k, data, df=(hd.freqs[1] - hd.freqs[0]), dt=((times[1] - times[0]) * 24. * 3600.))
                 np.testing.assert_array_equal(sigmasq, sigmasq2)
