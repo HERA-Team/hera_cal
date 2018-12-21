f8d40322fa7fc400ab54bb0daf1199d844d87708
Josh Dillon <jsdillon@berkeley.edu>
2018-12-21 12:47:10 -0800

    Create noise.py

---
+++ hera_cal/noise.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 the HERA Project
+# Licensed under the MIT License
+
+"""Module for predicting noise on visibilties using autocorrelations and for producing noise standard deviation data files."""
+
+from __future__ import print_function, division, absolute_import
+
+import numpy as np
+import argparse
+
