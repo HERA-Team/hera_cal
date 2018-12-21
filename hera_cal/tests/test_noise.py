886509a38800c987401ef30f0340f9b45578dab3
Josh Dillon <jsdillon@berkeley.edu>
2018-12-21 13:41:58 -0800

    add unit tests for noise.py

---
+++ hera_cal/tests/test_noise.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 the HERA Project
+# Licensed under the MIT License
+
+"""Unit tests for the hera_cal.apply_cal module."""
+
+from __future__ import absolute_import, division, print_function
+
+import unittest
+import numpy as np
+import os
+import sys
+
+from hera_cal import io
+from hera_cal import noise
+from hera_cal.data import DATA_PATH
+from hera_cal.utils import split_pol
+from hera_cal.apply_cal import apply_cal
+
+
+class Test_Noise(unittest.TestCase):
+
+    def test_per_antenna_noise_std(self):
+        infile = os.path.join(DATA_PATH, 'zen.2458098.43124.downsample.uvh5')
+        hd = io.HERAData(infile)
+        data, _, _ = hd.read()
+        n = noise.per_antenna_noise_std(data)
+        for bl in data.keys():
+            if (bl[0] == bl[1]) and (split_pol(bl[2])[0] == split_pol(bl[2])[1]):
+                self.assertTrue(bl in n)
+                self.assertEqual(n[bl].shape, data[bl].shape)
+                np.testing.assert_array_equal(n[bl].imag, 0.0)
+            else:
+                self.assertFalse(bl in n)
+
+    def test_write_per_antenna_noise_std_from_autos(self):
+        infile = os.path.join(DATA_PATH, 'zen.2458098.43124.downsample.uvh5')
+        calfile = os.path.join(DATA_PATH, 'zen.2458098.43124.downsample.omni.calfits')
+        outfile = os.path.join(DATA_PATH, 'test_output/noise.uvh5')
+        noise.write_per_antenna_noise_std_from_autos(infile, outfile, calfile=calfile, add_to_history='testing', clobber=True)
+
+        hd = io.HERAData(outfile)
+        n, f, _ = hd.read()
+        hc = io.HERACal(calfile)
+        g, gf, _, _ = hc.read()
+        for bl in n.keys():
+            self.assertEqual(bl[0], bl[1])
+            self.assertEqual(split_pol(bl[2])[0], split_pol(bl[2])[1])
+            np.testing.assert_array_equal(n[bl].imag, 0.0)
+            np.testing.assert_array_equal(f[bl], gf[bl[0], split_pol(bl[2])[0]])
+        os.remove(outfile)
+
+    def test_noise_std_argparser(self):
+        sys.argv = [sys.argv[0], 'a', 'b', '--calfile', 'd']
+        a = noise.noise_std_argparser()
+        args = a.parse_args()
+        self.assertEqual(args.infile, 'a')
+        self.assertEqual(args.outfile, 'b')
+        self.assertEqual(args.calfile, ['d'])
+
+
+if __name__ == '__main__':
+    unittest.main()
