0f6fcbe2f66e1a51506703e38bd6d606fef7044b
Josh Dillon <jsdillon@berkeley.edu>
2019-01-19 17:29:06 -0800

    remove abscal_funcs.fft_dly in favor of utils.fft_dly
    This closes #367

--- hera_cal/abscal_funcs.py
+++ hera_cal/abscal_funcs.py
@@ -27,7 +27,7 @@ from . import redcal
 from . import io
 from . import apply_cal
 from .datacontainer import DataContainer
-from .utils import polnum2str, polstr2num, jnum2str, jstr2num, reverse_bl, echo
+from .utils import polnum2str, polstr2num, jnum2str, jstr2num, reverse_bl, echo, fft_dly
 
 
 def abs_amp_logcal(model, data, wgts=None, verbose=True):
@@ -983,95 +983,6 @@ def array_axis_to_data_key(data, array_index, array_keys, key_index=-1, copy_dic
         return new_data, new_copy
     else:
         return new_data
-
-
-def fft_dly(data, df, wgts=None, window=None, medfilt=False, kernel=(1, 11),
-            solve_phase=False, edge_cut=0):
-    """Get delay of visibility across band using FFT and quadratic fit to delay peak.
-
-    Arguments:
-        data : ndarray of complex data (e.g. gains or visibilities) of shape (Ntimes, Nfreqs)
-        df : frequency channel width in Hz
-        wgts : multiplicative wgts of the same shape as the data
-        medfilt : boolean, median filter data before fft
-        kernel : size of median filter kernel along (time, freq) axes
-        window : str, window to enact on data before FFT, options=['blackmanharris', 'hann', None]
-            None is a top-hat window.
-        solve_phase=False
-        edge_cut : int, number of channels to exclude at each band edge of vis in FFT window
-
-    Returns:
-        dlys : (Ntimes, 1) ndarray containing delay for each integration
-        phi : ndarray containing phase of delay mode for each integration (or None if solve_phase is False)
-    """
-    Ntimes, Nfreqs = data.shape
-    if wgts is None:
-        wgts = np.ones_like(data, dtype=np.float)
-
-    # smooth via median filter
-    kernel += tuple(np.ones((data.ndim - len(kernel)), np.int))
-    if medfilt:
-        data = signal.medfilt(np.real(data), kernel_size=kernel) + 1j * signal.medfilt(np.imag(data), kernel_size=kernel)
-
-    # construct window
-    win = np.zeros_like(data, dtype=np.float)
-    assert 2 * edge_cut < Nfreqs - 1, "edge_cut cannot be >= Nfreqs/2 - 1"
-    if window is None:  # tophat window
-        win[:, edge_cut:(Nfreqs - edge_cut)] = 1.0
-    elif window == 'blackmanharris':
-        win[:, edge_cut:(Nfreqs - edge_cut)] = signal.windows.blackmanharris(Nfreqs - 2 * edge_cut)
-    elif window == 'hann':
-        win[:, edge_cut:(Nfreqs - edge_cut)] = signal.windows.hann(Nfreqs - 2 * edge_cut)
-    else:
-        raise ValueError("didn't recognize window {} from ['blackmanharris', 'hann', None]".format(window))
-
-    # fft w/ window and find argmax
-    vfft = np.fft.fft(data * win * wgts, axis=1)
-    amp = np.abs(np.fft.fftshift(vfft, axes=1))
-    argmaxes = np.argmax(amp, axis=1)
-    fftfreqs = np.fft.fftshift(np.fft.fftfreq(Nfreqs, df))
-    dlys, peak_shifts = np.zeros((Ntimes, 1)), np.zeros((Ntimes, 1))
-    # use parabolic peak interpolation: https://ccrma.stanford.edu/~jos/sasp/Quadratic_Interpolation_Spectral_Peaks.html
-    for i in range(Ntimes):
-        a, b, c, = amp[i][(argmaxes[i] - 1) % len(amp[i])], amp[i][argmaxes[i]], amp[i][(argmaxes[i] + 1) % len(amp[i])]
-        if (np.abs(a - 2 * b + c) > 0) and (np.abs(a - c) > 0):
-            peak_shifts[i] = .5 * (a - c) / (a - 2 * b + c)
-        else:
-            peak_shifts[i] = 0
-        # use peak shift to linearly interpolate to get appropriate delay
-        dlys[i] = (1.0 - np.abs(peak_shifts[i])) * fftfreqs[argmaxes[i]] + np.abs(peak_shifts[i]) * \
-                  (fftfreqs[argmaxes[i]] + np.sign(peak_shifts[i]) * (fftfreqs[1] - fftfreqs[0]))
-
-    phi = None
-    if solve_phase:
-        # get phase offsets by interpolating real and imag component of FFT
-        argmax = np.moveaxis(np.argmax(np.abs(vfft), axis=1)[np.newaxis], 0, 1)
-        vfft_real = []
-        vfft_imag = []
-        for i, a in enumerate(argmax):
-            # get real and imag of each argmax
-            real = np.take(vfft.real, i, axis=0)
-            imag = np.take(vfft.imag, i, axis=0)
-
-            # wrap around
-            real = np.concatenate([real, real, real])
-            imag = np.concatenate([imag, imag, imag])
-            a += Nfreqs
-
-            # add interpolation component
-            rl = interpolate.interp1d(np.arange(Nfreqs * 3), real)(a + peak_shifts[i])
-            im = interpolate.interp1d(np.arange(Nfreqs * 3), imag)(a + peak_shifts[i])
-
-            # insert into arrays
-            vfft_real.append(rl)
-            vfft_imag.append(im)
-
-        vfft_real = np.moveaxis(np.array(vfft_real), 0, 0)
-        vfft_imag = np.moveaxis(np.array(vfft_imag), 0, 0)
-        vfft_interp = vfft_real + 1j * vfft_imag
-        phi = np.angle(vfft_interp)
-
-    return dlys, phi
 
 
 def wiener(data, window=(5, 11), noise=None, medfilt=True, medfilt_kernel=(3, 9), array=False):
