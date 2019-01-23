07c582c3decc0ce5cbb24065afd88ac07e0c1b3f
Josh Dillon <jsdillon@berkeley.edu>
2019-01-23 15:57:05 -0800

    add post_redcal_abscal_run

--- hera_cal/abscal.py
+++ hera_cal/abscal.py
@@ -1645,3 +1645,138 @@ def post_redcal_abscal(model, data, flags, rc_flags, min_bl_cut=None, max_bl_cut
                 gain_convention=gain_convention, max_iter=phs_max_iter, conv_crit=phs_conv_crit, verbose=verbose)
 
     return abscal_delta_gains, AC
+
+
+def post_redcal_abscal_run(data_file, redcal_file, model_files, output_replace=('.omni.', '.abs.'), nInt_to_load=-1,
+                           data_solar_horizon=90, model_solar_horizon=90, min_bl_cut=1.0, max_bl_cut=None, edge_cut=0, 
+                           tol=1.0, phs_max_iter=100, phs_conv_crit=1e-6, clobber=True, add_to_history='', verbose=True):
+    '''Perform abscal on entire data files, picking relevant model_files from a list and doing partial data loading.
+    
+    Arguments:
+        data_file: string path to raw uvh5 visibility file
+        redcal_file: string path to calfits file that redundantly calibrates the data_file
+        model_files: list of string paths to externally calibrated data. Strings must be sortable 
+            to produce a chronological list in LST (wrapping over 2*pi is OK)
+        output_replace: tuple of two strings to find and replace in redcal_file to produce the output calfits file
+        nInt_to_load: number of integrations to load and calibrate simultaneously. Default -1 loads all integrations.
+        data_solar_horizon: Solar altitude threshold [degrees]. When the sun is too high in the data, flag the integration.
+        model_solar_horizon: Solar altitude threshold [degrees]. When the sun is too high in the model, flag the integration.
+        min_bl_cut: minimum baseline separation [meters] to keep in data when calibrating. None or 0 means no mininum,
+            which will include autocorrelations in the absolute calibration. Usually this is not desired, so the default is 1.0.
+        max_bl_cut: maximum baseline separation [meters] to keep in data when calibrating. None (default) means no maximum.
+        edge_cut: integer number of channels to exclude at each band edge in delay and global phase solvers
+        tol: baseline match tolerance in units of baseline vectors (e.g. meters)
+        phs_max_iter: integer maximum number of iterations of phase_slope_cal or TT_phs_cal allowed
+        phs_conv_crit: convergence criterion for updates to iterative phase calibration that compares them to all 1.0s.
+        clobber: if True, overwrites existing abscal calfits file at the output path
+        add_to_history: string to add to history of output abscal file
+
+    Returns:
+        hc: HERACal object which was written to disk. Matches the input redcal_file with an updated history and:
+            - gains: abscal gains for times that could be calibrated, redcal gains otherwise (but flagged)
+            - flags: redcal flags, with additional flagging if the data or model are flagged (see flag_utils.synthesize_ant_flags)
+            - quals: abscal chi^2 p
+            - total_qual: 
+    '''
+    # Raise error if output calfile already exists and clobber is False
+    if os.path.exists(redcal_file.replace(*output_replace)) and not clobber:
+        raise IOError("{} exists, not overwriting.".format(redcal_file.replace(*output_replace)))
+
+    # Load redcal calibration
+    hc = io.HERACal(redcal_file)
+    rc_gains, rc_flags, rc_quals, rc_tot_qual = hc.read()
+
+    # Initialize full-size, totally-flagged abscal gain/flag/etc. dictionaries
+    abscal_gains = copy.deepcopy(rc_gains)
+    abscal_flags = {ant: np.ones_like(rf) for ant, rf in rc_flags.items()}
+    abscal_chisq_per_ant = {ant: np.zeros_like(rq) for ant, rq in rc_quals.items()}
+    abscal_chisq = {pol: np.zeros_like(rtq) for pol, rtq in rc_tot_qual.items()}
+
+    # match times to narrow down model_files
+    matched_model_files = sorted(set(match_times(data_file, model_files, filetype='uvh5')))
+    if len(matched_model_files) > 0:
+        hd = io.HERAData(data_file)
+        hdm = io.HERAData(matched_model_files)
+        pol_load_list = [pol for pol in hd.pols if split_pol(pol)[0] == split_pol(pol)[1]]
+        
+        # match integrations in model to integrations in data
+        all_data_times, all_data_lsts = get_all_times_and_lsts(hd, solar_horizon=data_solar_horizon, unwrap=True)
+        all_model_times, all_model_lsts = get_all_times_and_lsts(hdm, solar_horizon=model_solar_horizon, unwrap=True)
+        d2m_time_map = get_d2m_time_map(all_data_times, all_data_lsts, all_model_times, all_model_lsts)
+        
+        # group matched time indices for partial I/O
+        matched_tinds = [tind for tind, time in enumerate(hd.times) if time in d2m_time_map and d2m_time_map[time] is not None]
+        if len(matched_tinds) > 0:
+            tind_groups = np.array([matched_tinds])  # just load a single group
+            if nInt_to_load > 0:  # split up the integrations to load nInt_to_load at a time
+                tind_groups = np.split(matched_tinds, np.arange(nInt_to_load, len(matched_tinds), nInt_to_load))
+
+            # loop over polarizations
+            for pol in pol_load_list:
+                echo('\n\nNow calibrating ' + pol + '-polarization...', verbose=verbose)
+                # loop over groups of time indices
+                for tinds in tind_groups:
+                    echo('\n    Now calibrating times ' + str(hd.times[tinds[0]]) + \
+                         ' through ' + str(hd.times[tinds[-1]]) + '...', verbose=verbose)
+                    
+                    # load data
+                    data, flags, nsamples = hd.read(times=hd.times[tinds], polarizations=[pol])
+                    if not np.all(flags.values()):
+                        # load model and rephase
+                        model_times_to_load = [d2m_time_map[time] for time in hd.times[tinds]]
+                        model, model_flags, _ = io.partial_time_io(hdm, model_times_to_load)
+                        model_bls = {bl: model.antpos[bl[0]] - model.antpos[bl[1]] for bl in model.keys()}
+                        utils.lst_rephase(model, model_bls, model.freqs, data.lsts - model.lsts,
+                                          lat=hdm.telescope_location_lat_lon_alt_degrees[0], inplace=True)
+                        
+                        # update data flags w/ model flags
+                        for k in flags.keys():
+                            if k in model_flags:
+                                flags[k] += model_flags[k]
+
+                        # apply calibration
+                        data_ants = set([ant for bl in data.keys() for ant in split_bl(bl)])
+                        rc_gains_subset = {k: rc_gains[k][tinds,:] for k in data_ants}
+                        rc_flags_subset = {k: rc_flags[k][tinds,:] for k in data_ants}
+                        calibrate_in_place(data, rc_gains_subset, data_flags=flags, 
+                                           cal_flags=rc_flags_subset, gain_convention=hc.gain_convention)
+                        
+                        # run absolute calibration, copying data because it gets modified internally
+                        delta_gains, AC = post_redcal_abscal(model, copy.deepcopy(data), flags, rc_flags_subset, edge_cut=edge_cut, 
+                                                             tol=tol, min_bl_cut=min_bl_cut, max_bl_cut=max_bl_cut, 
+                                                             gain_convention=hc.gain_convention, phs_max_iter=phs_max_iter, 
+                                                             phs_conv_crit=phs_conv_crit, verbose=verbose)
+
+                        # abscal data (AC.data is already abscaled, but data is only redcaled) and generate abscal Chi^2
+                        calibrate_in_place(data, delta_gains, data_flags=flags, 
+                                           cal_flags=rc_flags_subset, gain_convention=hc.gain_convention)
+                        chisq_wgts = {}
+                        for bl in AC.data.keys():
+                            dt = (np.median(np.ediff1d(hd.times_by_bl[bl[:2]])) * 86400.)
+                            noise_var = predict_noise_variance_from_autos(bl, data, dt=dt)
+                            chisq_wgts[bl] = noise_var**-1 * (~flags[bl]).astype(np.float)
+                        total_qual, nObs, quals, nObs_per_ant = utils.chisq(AC.data, AC.model, chisq_wgts,
+                                                                            gain_flags=rc_flags_subset, split_by_antpol=True)
+                    
+                        # update results
+                        delta_flags = synthesize_ant_flags(flags)
+                        for ant in data_ants:
+                            abscal_gains[ant][tinds, :] = rc_gains_subset[ant] * delta_gains[ant]
+                            abscal_flags[ant][tinds, :] = rc_flags_subset[ant] + delta_flags[ant]
+                            abscal_chisq_per_ant[ant][tinds, :] = quals[ant] / nObs_per_ant[ant]  # Note, not normalized for DoF
+                        for antpol in total_qual.keys():
+                            abscal_chisq[antpol][tinds, :] = total_qual[antpol] / nObs[antpol]  # Note, not normalized for DoF
+                            
+        # impose a single reference antenna on the final antenna solution
+        refant=pick_reference_antenna(abscal_flags)
+        refant_phasor = (abscal_gains[refant] / np.abs(abscal_gains[refant]))
+        for ant in abscal_gains.keys():
+            abscal_gains[ant] /= refant_phasor
+    else:
+        echo("No model files overlap with data files in LST. Result will be fully flagged.", verbose=verbose)
+
+    # Save results to disk
+    hc.update(gains=abscal_gains, flags=abscal_flags, quals=abscal_chisq_per_ant, total_qual=abscal_chisq)
+    hc.history += version.history_string(add_to_history)
+    hc.write_calfits(redcal_file.replace(*output_replace), clobber=clobber)
+    return hc
