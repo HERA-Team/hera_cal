f8ec8b3ac0751ccfec8d1f82bbfd9e84adb1f629
Josh Dillon <jsdillon@berkeley.edu>
2019-03-13 18:24:39 -0700

    enable command-line partial i/o apply_cal

--- hera_cal/apply_cal.py
+++ hera_cal/apply_cal.py
@@ -186,6 +186,7 @@ def apply_cal_argparser():
     a.add_argument("--flag_nchan_high", type=int, default=0, help="integer number of channels at the high frequency end of the band to always flag (default 0)")
     a.add_argument("--filetype_in", type=str, default='miriad', help='filetype of input data files')
     a.add_argument("--filetype_out", type=str, default='miriad', help='filetype of output data files')
+    a.add_argument("--nbl_per_load", type=int, defualt=None, help="Maximum number of baselines to load at once. uvh5 to uvh5 only. Default loads the whole file.")
     a.add_argument("--gain_convention", type=str, default='divide',
                    help="'divide' means V_obs = gi gj* V_true, 'multiply' means V_true = gi gj* V_obs.")
     a.add_argument("--clobber", default=False, action="store_true", help='overwrites existing file at outfile')
