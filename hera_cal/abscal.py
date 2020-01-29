49b84687137b1eb2915462fe4d6ed68901017ba1
Josh Dillon <jsdillon@berkeley.edu>
2020-01-28 16:32:31 -0800

    pep8

--- hera_cal/abscal.py
+++ hera_cal/abscal.py
@@ -2856,10 +2856,9 @@ def post_redcal_abscal(model, data, flags, rc_flags, min_bl_cut=None, max_bl_cut
     idealized_antpos = redcal.reds_to_antpos(redcal.get_reds(data.antpos, bl_error_tol=tol), tol=IDEALIZED_BL_TOL)
     if np.max([len(pos) for pos in idealized_antpos.values()]) > 2:  # the array is not redundant (i.e. extra degeneracies)
         suspected_off_grid = [ant for ant, pos in idealized_antpos.items() if np.any(np.abs(pos[2:]) > IDEALIZED_BL_TOL)]
-        ex_ants = [ant for ant in idealized_antpos if np.all([np.all(f) for a, f in rc_flags.items() if ant in a])]
+        not_flagged = [ant for ant in suspected_off_grid if not np.all([np.all(f) for a, f in rc_flags.items() if ant in a])]
         warnings.warn(('WARNING: The following antennas appear not to be redundant with the main array:\n         {}\n'
-                       '         Of them, {} is not flagged.\n').format(suspected_off_grid, 
-                                                                     [ant for ant in suspected_off_grid if ant not in ex_ants]))
+                       '         Of them, {} is not flagged.\n').format(suspected_off_grid, not_flagged))
         idealized_antpos = {ant: pos[:2] for ant, pos in idealized_antpos.items()}
     AC._set_antpos(idealized_antpos)
 
