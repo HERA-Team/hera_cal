aaee058407e7e031cbd3a2ea5c833285d2c32f03
Aaron Ewall-Wice <aewallwi@herapost-master.aoc.nrao.edu>
2020-02-18 00:33:25 -0700

    info formatting change.'

--- hera_cal/smooth_cal.py
+++ hera_cal/smooth_cal.py
@@ -105,24 +105,28 @@ def freq_filter(gains, wgts, freqs, filter_scale=10.0, tol=1e-09, window='tukey'
         filtered, res, info = uvtools.dspec.high_pass_fourier_filter(gains * rephasor, wgts, filter_size, df, tol=tol, window=window,
                                                                  skip_wgt=skip_wgt, maxiter=maxiter, fitting_options=fitting_options,
                                                                  cache = cache)
+        # put back in unfilted values if skip_wgt is triggered
+        for i, info_dict in enumerate(info):
+            if info_dict.get('skipped', False):
+                filtered[i, :] = gains[i, :]
 
 
     elif mode in ['dpss_leastsq', 'dft_leastsq']:
         if cache is None:
             cache = {}
         if fitting_options is None:
-            raise ValueError("filter_options must be supplied for dpss or dft interpolation.")
+            raise ValueError("fiting_options must be supplied for dpss or dft interpolation.")
 
         filtered, res, info = uvtools.dspec.fourier_filter(x=freqs, data=gains*rephasor, wgts=wgts, filter_centers=[0.],
                                                             filter2d=False, filter_dim=1, filter_half_widths=[1. / (filter_scale * 10 ** 6)],
                                                             mode=mode, skip_wgt=skip_wgt, fitting_options=fitting_options, suppression_factors=[tol])
         info = info[1]
+        # put back in unfilted values if skip_wgt is triggered
+        for i in info:
+            if not info[i] == 'skipped':
+                filtered[i, :] = gains[i, :]
 
     filtered /= rephasor
-    # put back in unfilted values if skip_wgt is triggered
-    for i, info_dict in enumerate(info):
-        if info_dict.get('skipped', False):
-            filtered[i, :] = gains[i, :]
 
 
     return filtered, info
