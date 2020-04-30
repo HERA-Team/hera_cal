e3586e6d34ce109edfc366e6857900d3972c2cb2
Josh Dillon <jsdillon@berkeley.edu>
2020-04-29 18:59:16 -0700

    add test for blacklist parsing

--- hera_cal/tests/test_smooth_cal.py
+++ hera_cal/tests/test_smooth_cal.py
@@ -30,10 +30,11 @@ class Test_Smooth_Cal_Helper_Functions(object):
         assert len(kernel) == 201
 
     def test_smooth_cal_argparser(self):
-        sys.argv = [sys.argv[0], 'a', 'b', '--flag_file_list', 'c']
+        sys.argv = [sys.argv[0], 'a', 'b', '--flag_file_list', 'c', '--lst_blacklists', '3-4', '10-12', '23-.5']
         a = smooth_cal.smooth_cal_argparser()
         assert a.calfits_list == ['a', 'b']
         assert a.flag_file_list == ['c']
+        assert a.lst_blacklists == [(3, 4), (10, 12), (23, .5)]
 
     def test_time_filter(self):
         gains = np.ones((10, 10), dtype=complex)
