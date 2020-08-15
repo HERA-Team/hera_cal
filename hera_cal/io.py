6dcd6f61b3f8dd2a81fb18258220b35c94dace62
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-08-14 20:12:45 -0700

    the things we do for coverage...

--- hera_cal/io.py
+++ hera_cal/io.py
@@ -634,7 +634,28 @@ class HERAData(UVData):
                 if self.filetype != 'uvh5':
                     raise NotImplementedError('Redundant group iteration without explicitly setting antpos for filetype ' + self.filetype
                                               + ' without setting antpos has not been implemented.')
-                reds = redcal.get_reds(self.antpos, pols=self.pols, bl_error_tol=bl_error_tol,
+                antpos = {}
+                pols = []
+                # generate antpos dict to feed into get_reds
+                # that accounts for possibility that
+                # HERACal was initialized from multiple
+                # files in which case self.antpos is a dict of dicts.
+                for ant in self.antpos:
+                    if isinstance(self.antpos[ant], dict):
+                        for k in self.antpos[ant]:
+                            antpos[k] = self.antpos[ant][k]
+                    else:
+                        antpos[ant] = self.antpos[ant]
+                # generate pol list with same considerations.
+                if isinstance(self.pols, list):
+                    pols = self.pols
+                else:
+                    for k in self.pols:
+                        for pol in self.pols[k]:
+                            if pol not in pols:
+                                pols.append(pol)
+
+                reds = redcal.get_reds(antpos, pols=pols, bl_error_tol=bl_error_tol,
                                        include_autos=True)
             baseline_chunks = chunk_baselines_by_redundant_groups(bls=bls, reds=reds, max_chunk_size=Nbls)
         for chunk in baseline_chunks:
--- hera_cal/tests/test_io.py
+++ hera_cal/tests/test_io.py
@@ -480,6 +480,20 @@ class Test_HERAData(object):
         with pytest.raises(NotImplementedError):
             next(hd.iterate_over_bls())
 
+        hd = HERAData(self.uvh5_1)
+        for (d, f, n) in hd.iterate_over_bls(chunk_by_redundant_group=True):
+            for dc in (d, f, n):
+                assert list(d.values())[0].shape == (60, 1024)
+
+        hd = HERAData([self.uvh5_1, self.uvh5_2])
+        for (d, f, n) in hd.iterate_over_bls(chunk_by_redundant_group=True):
+            for dc in (d, f, n):
+                assert list(d.values())[0].shape == (120, 1024)
+
+        with pytest.raises(NotImplementedError):
+            hd = HERAData(self.miriad_1, filetype='miriad')
+            d, f, n = next(hd.iterate_over_bls(bls=[(52, 53, 'xx')], chunk_by_redundant_group=True))
+
     def test_iterate_over_freqs(self):
         hd = HERAData(self.uvh5_1)
         for (d, f, n) in hd.iterate_over_freqs(Nchans=256):
