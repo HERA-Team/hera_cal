ff5e15fe102e831fa98ef688aba0cfccb32d68d6
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-08-15 22:32:17 -0700

    move file reconstitution to vis_clean

--- scripts/reconstitute_filtered_files_run.py
+++ scripts/reconstitute_filtered_files_run.py
@@ -3,15 +3,15 @@
 # Copyright 2020 the HERA Project
 # Licensed under the MIT License
 
-"Command-line drive script for hera_cal.xtalk_filter. Only performs DAYENU Filtering"
+"Command line driver for changing from files chunking by baseline to files chunking by time.""
 
 from hera_cal import xtalk_filter
 
-parser = xtalk_filter.reconstitute_xtalk_files_argparser()
+parser = xtalk_filter.reconstitute_files_argparser()
 
 a = parser.parse_args()
 
 # Run Xtalk Filter
-xtalk_filter.reconstitute_xtalk_files(templatefile=a.infilename,
+xtalk_filter.reconstitute_files(templatefile=a.infilename,
                                       fragments=a.fragmentlist,
                                       outfilename=a.outfilename)
