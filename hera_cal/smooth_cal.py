fix the averaging in freq_filter.

hera_cal/smooth_cal.py
@@ -105,7 +105,8 @@ def freq_filter(gains, wgts, freqs, filter_scale=10.0, skip_wgt=0.1,
     # if broadcast_time_average is true, then set all times to
     # the average of the solutions.
     if broadcast_time_average:
-        time_average = np.mean(filtered, axis=0)
+        skipwgts = np.asarray([info['status']['axis_1'][i] != 'skipped' for i in info['status']['axis_1']]).astype(float)
+        time_average = np.sum(filtered * skip_wgts[:, None], axis=0) / np.sum(skipwgts)
         for m in range(filtered.shape[0]):
             filtered[m] = time_average
     else: