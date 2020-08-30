7c730149a2bb83f163df0476497c9d2f8e1a4fca
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-08-30 13:17:00 -0700

    add drive script for simple smooth_cal

--- scripts/smooth_cal_timeavg_run.py
+++ scripts/smooth_cal_timeavg_run.py
@@ -3,7 +3,10 @@
 # Copyright 2019 the HERA Project
 # Licensed under the MIT License
 
-"""Command-line drive script for hera_cal.smooth_cal. This script
+"""Command-line drive script for hera_cal.smooth_cal.
+That performs per-time gain fitting over a subband
+and broadcasts the average to all times.
+This script also supports flag factorization.
 1) loads in the calibration solutions and (optionally) the associated flagging npzs for (usually) a whole day
 2) performs a 2D time and frequency smoothing of the calibration solutions using aipy.deconv.clean (default scales 1800 s and 10 MHz)
 4) writes the smoothed calibrations solutions to disk.
@@ -13,19 +16,17 @@ See help for a more detailed explanation of the parameters.
 from hera_cal.smooth_cal import CalibrationSmoother, smooth_cal_argparser
 import sys
 
-a = smooth_cal_argparser()
-win_kwargs = {}
-if a.window == 'tukey':  # set window kwargs
-    win_kwargs['alpha'] = a.alpha
+a = smooth_cal_argparser(mode='dpss_freqfilter_timeaverage')
 
 if a.run_if_first is None or sorted(a.calfits_list)[0] == a.run_if_first:
     cs = CalibrationSmoother(a.calfits_list, flag_file_list=a.flag_file_list, flag_filetype=a.flag_filetype,
                              antflag_thresh=a.antflag_thresh, time_blacklists=a.time_blacklists,
                              lst_blacklists=a.lst_blacklists, freq_blacklists=a.freq_blacklists,
                              chan_blacklists=a.chan_blacklists, pick_refant=a.pick_refant, freq_threshold=a.freq_threshold,
-                             time_threshold=a.time_threshold, ant_threshold=a.ant_threshold, verbose=a.verbose)
-    cs.time_freq_2D_filter(freq_scale=a.freq_scale, time_scale=a.time_scale, tol=a.tol,
-                           filter_mode=a.filter_mode, window=a.window, maxiter=a.maxiter, **win_kwargs)
+                             time_threshold=a.time_threshold, ant_threshold=a.ant_threshold, verbose=a.verbose,
+                             factorize_flags=a.factorize_flags)
+    cs.freq_filter(filter_scale=a.freq_scale, mode='dpss_leastsq',
+                   broadcast_time_average=True)
     cs.write_smoothed_cal(output_replace=(a.infile_replace, a.outfile_replace),
                           add_to_history=' '.join(sys.argv), clobber=a.clobber)
 else:
