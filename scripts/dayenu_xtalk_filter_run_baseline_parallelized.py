baba039820f5a237bdbf7a1aed3c2cce8f019b03
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-08-31 21:23:21 -0700

    fix script typo.

--- scripts/dayenu_xtalk_filter_run_baseline_parallelized.py
+++ scripts/dayenu_xtalk_filter_run_baseline_parallelized.py
@@ -20,9 +20,9 @@ baseline_list = io.baselines_from_filelist_position(filename=a.infilename, filel
 outfilename = a.res_outfilename
 spw_range = a.spw_range
 # allow none string to be passed through to a.calfile
-if a.calfile_list is not None:
-    if a.calfile_list.lower() == 'none':
-        a.calfile_list = None
+if a.calfilelist is not None:
+    if a.calfilelist.lower() == 'none':
+        a.calfilelist = None
 # Run Xtalk Filter
 xtalk_filter.load_xtalk_filter_and_write_baseline_list(a.datafilelist, calfile_list=a.calfilelist, round_up_bllens=True,
                                                        baseline_list=baseline_list, spw_range=a.spw_range,
