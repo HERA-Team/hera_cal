86ca10c204d80fce58d32868884add6c95b90dfb
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-09-12 22:22:43 -0700

    update scripts.

--- scripts/dayenu_xtalk_filter_run.py
+++ scripts/dayenu_xtalk_filter_run.py
@@ -23,4 +23,5 @@ xtalk_filter.load_xtalk_filter_and_write(a.infilename, calfile=a.calfile, round_
                                          read_cache=a.read_cache, mode='dayenu',
                                          factorize_flags=a.factorize_flags, time_thresh=a.time_thresh,
                                          trim_edges=a.trim_edges, max_contiguous_edge_flags=a.max_contiguous_edge_flags,
-                                         add_to_history=' '.join(sys.argv), **filter_kwargs)
+                                         add_to_history=' '.join(sys.argv),
+                                         skip_flagged_edges=a.skip_flagged_edges, **filter_kwargs)
