62adda86040e5827ca4865e33d2185a4b46f9e68
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-09-17 10:43:09 -0700

    update even / odd script.

--- scripts/sum_diff_2_even_odd.py
+++ scripts/sum_diff_2_even_odd.py
@@ -8,6 +8,9 @@ from hera_cal import apply_cal
 a = apply_cal.sum_diff_2_even_odd_argparser()
 args = a.parse_args()
 
+if args.nbl_per_load == "none":
+    args.nbl_per_load = None
+
 apply_cal.sum_diff_2_even_odd(args.sumfilename, args.difffilename,
                               args.evenfilename, args.oddfilename,
                               nbl_per_load=args.nbl_per_load)
