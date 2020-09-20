54174051d28678e35807264fd2c52cf5d3e01483
Josh Dillon <jsdillon@berkeley.edu>
2020-09-20 14:17:51 -0700

    fix bug

--- scripts/apply_cal.py
+++ scripts/apply_cal.py
@@ -20,5 +20,5 @@ if args.vis_units is not None:
 ac.apply_cal(args.infilename, args.outfilename, args.new_cal, old_calibration=args.old_cal, flag_file=args.flag_file,
              flag_filetype=args.flag_filetype, flag_nchan_low=args.flag_nchan_low, flag_nchan_high=args.flag_nchan_high,
              filetype_in=args.filetype_in, filetype_out=args.filetype_out, nbl_per_load=args.nbl_per_load,
-             gain_convention=args.gain_convention, redundant_solution=args.redundant_solution, redundant_average=a.redundant_average,
+             gain_convention=args.gain_convention, redundant_solution=args.redundant_solution, redundant_average=args.redundant_average,
              add_to_history=' '.join(sys.argv), clobber=args.clobber, **kwargs)
