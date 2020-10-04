c57c8817ad8738fc1e2872850f8e66e08beddce9
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-10-04 16:15:12 -0700

    handle empty baseline chunks.

--- scripts/dayenu_delay_filter_run_baseline_parallelized.py
+++ scripts/dayenu_delay_filter_run_baseline_parallelized.py
@@ -15,19 +15,20 @@ a = parser.parse_args()
 filter_kwargs = {'standoff': a.standoff, 'horizon': a.horizon, 'tol': a.tol,
                  'skip_wgt': a.skip_wgt, 'min_dly': a.min_dly}
 baseline_list = io.baselines_from_filelist_position(filename=a.infilename, filelist=a.datafilelist)
-outfilename = a.res_outfilename
-spw_range = a.spw_range
-# allow none string to be passed through to a.calfile
-if a.calfile_list is not None:
-    if a.calfile_list.lower() == 'none':
-        a.calfile_list = None
-# Run Delay Filter
-delay_filter.load_delay_filter_and_write(a.datafilelist, calfile_list=a.calfile_list, round_up_bllens=True,
-                                         baseline_list=baseline_list, spw_range=a.spw_range,
-                                         cache_dir=a.cache_dir, res_outfilename=a.res_outfilename,
-                                         clobber=a.clobber, write_cache=a.write_cache,
-                                         read_cache=a.read_cache, mode='dayenu',
-                                         factorize_flags=a.factorize_flags, time_thresh=a.time_thresh,
-                                         trim_edges=a.trim_edges, max_contiguous_edge_flags=a.max_contiguous_edge_flags,
-                                         add_to_history=' '.join(sys.argv),
-                                         skip_flagged_edges=a.skip_flagged_edges, **filter_kwargs)
+if len(baseline_list) > 0:
+    outfilename = a.res_outfilename
+    spw_range = a.spw_range
+    # allow none string to be passed through to a.calfile
+    if a.calfile_list is not None:
+        if a.calfile_list.lower() == 'none':
+            a.calfile_list = None
+    # Run Delay Filter
+    delay_filter.load_delay_filter_and_write(a.datafilelist, calfile_list=a.calfile_list, round_up_bllens=True,
+                                             baseline_list=baseline_list, spw_range=a.spw_range,
+                                             cache_dir=a.cache_dir, res_outfilename=a.res_outfilename,
+                                             clobber=a.clobber, write_cache=a.write_cache,
+                                             read_cache=a.read_cache, mode='dayenu',
+                                             factorize_flags=a.factorize_flags, time_thresh=a.time_thresh,
+                                             trim_edges=a.trim_edges, max_contiguous_edge_flags=a.max_contiguous_edge_flags,
+                                             add_to_history=' '.join(sys.argv),
+                                             skip_flagged_edges=a.skip_flagged_edges, **filter_kwargs)
