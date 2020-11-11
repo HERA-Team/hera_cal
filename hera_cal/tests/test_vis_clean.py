3699b90855f17eabd1f13df87e39face3b553cbb
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-11-11 15:34:32 -0800

    codestyle fixes.

--- hera_cal/tests/test_vis_clean.py
+++ hera_cal/tests/test_vis_clean.py
@@ -441,7 +441,6 @@ class Test_VisClean(object):
         n = vis_clean.noise_eq_bandwidth(dspec.gen_window('blackmanharris', 10000))
         assert np.isclose(n, 1.9689862471203075)
 
-
     def test_zeropad(self):
         fname = os.path.join(DATA_PATH, "zen.2458043.40141.xx.HH.XRAA.uvh5")
         V = VisClean(fname, filetype='uvh5')
