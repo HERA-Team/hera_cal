bb5246f047703c9a753d186cabf013481310b219
Aaron Ewall-Wice <aaronew@berkeley.edu>
2020-11-24 16:29:00 -0800

    fix up ant num selection.

--- hera_cal/io.py
+++ hera_cal/io.py
@@ -137,9 +137,12 @@ class HERACal(UVCal):
             if pols is not None:
                 pols = [jstr2num(ap, x_orientation=self.x_orientation) for ap in pols]
             # only read antennas present in the data and raise a warning.
-            antenna_nums_in_cal = np.intersect1d(self.antenna_nums, antenna_nums)
+            if antenna_nums is None:
+                antenna_nums = np.unique(self.ant_array)
+            my_ants = np.unique(self.ant_array),
+            antenna_nums_in_cal = np.intersect1d(my_ants, antenna_nums)
             for ant in antenna_nums:
-                if not ant in self.antenna_nums:
+                if not ant in my_ants:
                     warnings.warn(f"Warning, antenna {ant} not present in calibration solution. Skipping!")
             select_dict = {'antenna_nums': antenna_nums_in_cal, 'frequencies': frequencies,
                            'freq_chans': freq_chans, 'jones': pols}
