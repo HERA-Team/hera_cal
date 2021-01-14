2531ece8c25e81feeb7694272601fe4606193fd0
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-01-14 11:00:08 -0800

    consolidate delay scripts.

--- scripts/delay_filter_run.py
+++ scripts/delay_filter_run.py
@@ -8,23 +8,53 @@
 from hera_cal import delay_filter
 import sys
 
-parser = delay_filter.delay_filter_argparser(mode='clean')
+parser = delay_filter.delay_filter_argparser()
 
 a = parser.parse_args()
-
 # set kwargs
-filter_kwargs = {'standoff': a.standoff, 'horizon': a.horizon, 'tol': a.tol, 'window': a.window,
-                 'skip_wgt': a.skip_wgt, 'maxiter': a.maxiter, 'edgecut_hi': a.edgecut_hi,
-                 'edgecut_low': a.edgecut_low, 'min_dly': a.min_dly, 'gain': a.gain}
-if a.window == 'tukey':
-    filter_kwargs['alpha'] = a.alpha
-spw_range = a.spw_range
+if a.mode == 'clean':
+    filter_kwargs = {'standoff': a.standoff, 'horizon': a.horizon, 'tol': a.tol, 'window': a.window,
+                     'skip_wgt': a.skip_wgt, 'maxiter': a.maxiter, 'edgecut_hi': a.edgecut_hi,
+                     'edgecut_low': a.edgecut_low, 'min_dly': a.min_dly, 'gain': a.gain}
+    if a.window == 'tukey':
+        filter_kwargs['alpha'] = a.alpha
+    round_up_bllens=False
+    skip_flags_larger_then_filter_period=False
+    skip_flagged_edges=False
+    max_contiguous_edge_flags=10000
+elif a.mode == 'dayenu':
+    filter_kwargs = {'standoff': a.standoff, 'horizon': a.horizon, 'tol': a.tol,
+                     'skip_wgt': a.skip_wgt, 'min_dly': a.min_dly}
+    round_up_bllens=True
+    max_contiguous_edge_flags=10000
+    skip_flags_larger_then_filter_period=False
+elif a.mode == 'dpss_leastsq':
+    filter_kwargs = {'standoff': a.standoff, 'horizon': a.horizon, 'tol': a.tol,
+                     'skip_wgt': a.skip_wgt, 'min_dly': a.min_dly}
+    round_up_bllens=True
+    skip_flags_larger_then_filter_period=True
+    skip_flagged_edges=True
+    max_contiguous_edge_flags=1
+else:
+    raise ValueError(f"mode {mode} not supported.")
+if a.calfile is not None:
+    if a.calfile.lower() == 'none':
+        a.calfile = None
+
 # Run Delay Filter
-delay_filter.load_delay_filter_and_write(a.infilename, calfile=a.calfile, Nbls_per_load=a.partial_load_Nbls, verbose=a.verbose,
-                                         res_outfilename=a.res_outfilename, CLEAN_outfilename=a.CLEAN_outfilename,
-                                         filled_outfilename=a.filled_outfilename, clobber=a.clobber, spw_range=spw_range,
-                                         add_to_history=' '.join(sys.argv),
+delay_filter.load_delay_filter_and_write(a.infilename, calfile=a.calfile, round_up_bllens=round_up_bllens,
+                                         Nbls_per_load=a.partial_load_Nbls, spw_range=a.spw_range,
+                                         cache_dir=a.cache_dir, res_outfilename=a.res_outfilename,
+                                         clobber=a.clobber, write_cache=a.write_cache,
+                                         read_cache=a.read_cache, mode=mode,
+                                         filled_outfilename=a.filled_outfilename,
+                                         CLEAN_outfilename=a.CLEAN_outfilename,
+                                         factorize_flags=a.factorize_flags, time_thresh=a.time_thresh,
+                                         max_contiguous_edge_flags=max_contiguous_edge_flags,
+                                         add_to_history=' '.join(sys.argv), verbose=a.verbose,
+                                         skip_flagged_edges=skip_flagged_edges,
                                          a_priori_flag_yaml=a.a_priori_flag_yaml,
                                          external_flags=a.external_flags,
+                                         skip_gaps_larger_then_filter_period=skip_gaps_larger_then_filter_period,
                                          overwrite_data_flags=a.overwrite_data_flags,
-                                         **filter_kwargs)
+                                         clean_flags_in_resid_flags=True, **filter_kwargs)
