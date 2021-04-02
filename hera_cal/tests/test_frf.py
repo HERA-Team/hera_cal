3f61040aaa4d8079376756d14e041e63e8600a53
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-04-02 14:33:07 -0700

    unittests.

--- hera_cal/tests/test_frf.py
+++ hera_cal/tests/test_frf.py
@@ -243,3 +243,306 @@ class Test_FRFilter(object):
         assert not args.verbose
         assert args.flag_output is None
         assert args.filetype == "uvh5"
+
+    def test_run_fr_filter(self):
+        fname = os.path.join(DATA_PATH, "zen.2458043.12552.xx.HH.uvORA")
+        k = (24, 25, 'ee')
+        frfil = frf.FRFilter(fname, filetype='miriad')
+        frfil.read(bls=[k])
+        bl = np.linalg.norm(frfil.antpos[24] - frfil.antpos[25]) / constants.c * 1e9
+        sdf = (frfil.freqs[1] - frfil.freqs[0]) / 1e9
+
+        frfil.run_fr_filter(to_filter=frfil.data.keys(), tol=1e-2)
+        for k in frfil.data.keys():
+            assert frfil.clean_resid[k].shape == (60, 64)
+            assert frfil.clean_model[k].shape == (60, 64)
+            assert k in frfil.clean_info
+
+        # test skip_wgt imposition of flags
+        fname = os.path.join(DATA_PATH, "zen.2458043.12552.xx.HH.uvORA")
+        k = (24, 25, 'ee')
+        # check successful run when avg_red_bllens is True and when False.
+        for avg_red_bllens in [True, False]:
+            frfil = frf.XTalkFilter(fname, filetype='miriad')
+            frfil.read(bls=[k])
+            if avg_red_bllens:
+                frfil.avg_red_baseline_vectors()
+            wgts = {k: np.ones_like(frfil.flags[k], dtype=np.float)}
+            wgts[k][:, 0] = 0.0
+            frfil.run_fr_filter(to_filter=[k], weight_dict=wgts, tol=1e-5, window='blackman-harris', skip_wgt=0.1, maxiter=100)
+            assert frfil.clean_info[k]['status']['axis_0'][0] == 'skipped'
+            np.testing.assert_array_equal(frfil.clean_flags[k][:, 0], np.ones_like(frfil.flags[k][:, 0]))
+            np.testing.assert_array_equal(frfil.clean_model[k][:, 0], np.zeros_like(frfil.clean_resid[k][:, 0]))
+            np.testing.assert_array_equal(frfil.clean_resid[k][:, 0], np.zeros_like(frfil.clean_resid[k][:, 0]))
+
+    def test_load_fr_filter_and_write_baseline_list(self, tmpdir):
+        tmp_path = tmpdir.strpath
+        uvh5 = [os.path.join(DATA_PATH, "test_input/zen.2458101.46106.xx.HH.OCR_53x_54x_only.first.uvh5"),
+                os.path.join(DATA_PATH, "test_input/zen.2458101.46106.xx.HH.OCR_53x_54x_only.second.uvh5")]
+        cals = [os.path.join(DATA_PATH, "test_input/zen.2458101.46106.xx.HH.uv.abs.calfits_54x_only.part1"),
+                os.path.join(DATA_PATH, "test_input/zen.2458101.46106.xx.HH.uv.abs.calfits_54x_only.part2")]
+        outfilename = os.path.join(tmp_path, 'temp.h5')
+        cdir = os.path.join(tmp_path, 'cache_temp')
+        # make a cache directory
+        if os.path.isdir(cdir):
+            shutil.rmtree(cdir)
+        os.mkdir(cdir)
+        for avg_bl in [True, False]:
+            frf.load_fr_filter_and_write_baseline_list(datafile_list=uvh5, baseline_list=[(53, 54, 'ee')],
+                                                       calfile_list=cals, spw_range=[100, 200], cache_dir=cdir,
+                                                       read_cache=True, write_cache=True, avg_red_bllens=avg_bl,
+                                                       res_outfilename=outfilename, clobber=True,
+                                                       mode='dpss_leastsq')
+            hd = io.HERAData(outfilename)
+            d, f, n = hd.read()
+            assert len(list(d.keys())) == 1
+            assert d[(53, 54, 'ee')].shape[1] == 100
+            assert d[(53, 54, 'ee')].shape[0] == 60
+            # now do no spw range and no cal files just to cover those lines.
+            frf.load_fr_filter_and_write_baseline_list(datafile_list=uvh5, baseline_list=[(53, 54, 'ee')],
+                                                         cache_dir=cdir,
+                                                         read_cache=True, write_cache=True, avg_red_bllens=avg_bl,
+                                                         res_outfilename=outfilename, clobber=True,
+                                                         mode='dpss_leastsq')
+            hd = io.HERAData(outfilename)
+            d, f, n = hd.read()
+            assert len(list(d.keys())) == 1
+            assert d[(53, 54, 'ee')].shape[1] == 1024
+            assert d[(53, 54, 'ee')].shape[0] == 60
+        # now test flag factorization and time thresholding.
+        # prepare an input files for broadcasting flags
+        uvh5 = os.path.join(DATA_PATH, "test_input/zen.2458101.46106.xx.HH.OCR_53x_54x_only.uvh5")
+        input_file = os.path.join(tmp_path, 'temp_special_flags.h5')
+        shutil.copy(uvh5, input_file)
+        hd = io.HERAData(input_file)
+        _, flags, _ = hd.read()
+        ntimes_before = hd.Ntimes
+        nfreqs_before = hd.Nfreqs
+        freqs_before = hd.freqs
+        times_before = hd.times
+        for bl in flags:
+            flags[bl][:] = False
+            flags[bl][0, :hd.Nfreqs // 2] = True  # first time has 50% flagged
+            flags[bl][-3:, -1] = True  # last channel has flags for three integrations
+        hd.update(flags=flags)
+        hd.write_uvh5(input_file, clobber=True)
+        # this time_threshold will result in
+        # entire first integration begin flagged
+        # and entire final channel being flagged
+        # when flags are broadcasted.
+        time_thresh = 2. / hd.Ntimes
+        for blnum, bl in enumerate(flags.keys()):
+            outfilename = os.path.join(tmp_path, 'bl_chunk_%d.h5' % blnum)
+            frf.load_fr_filter_and_write_baseline_list(datafile_list=[input_file], res_outfilename=outfilename,
+                                                          tol=1e-4, baseline_list=[bl],
+                                                          cache_dir=cdir,
+                                                          factorize_flags=True, time_thresh=time_thresh, clobber=True)
+        # now load all of the outputs in
+        output_files = glob.glob(tmp_path + '/bl_chunk_*.h5')
+        hd = io.HERAData(output_files)
+        d, f, n = hd.read()
+        hd_original = io.HERAData(uvh5)
+        for bl in hd_original.bls:
+            assert bl in d.keys()
+
+        for bl in f:
+            assert np.all(f[bl][:, -1])
+            assert np.all(f[bl][0, :])
+
+        # test apriori flags and flag_yaml
+        flag_yaml = os.path.join(DATA_PATH, 'test_input/a_priori_flags_sample.yaml')
+        uvf = UVFlag(hd, mode='flag', copy_flags=True)
+        uvf.to_waterfall(keep_pol=False, method='and')
+        uvf.flag_array[:] = False
+        flagfile = os.path.join(tmp_path, 'test_flag.h5')
+        uvf.write(flagfile, clobber=True)
+        frf.load_fr_filter_and_write_baseline_list(datafile_list=[input_file], res_outfilename=outfilename,
+                                                  tol=1e-4, baseline_list=[bl[:2]],
+                                                  clobber=True, mode='dpss_leastsq',
+                                                  external_flags=flagfile, overwrite_flags=True)
+        # test that all flags are False
+        hd = io.HERAData(outfilename)
+        d, f, n = hd.read()
+        for k in f:
+            assert np.all(~f[k])
+        # now do the external yaml
+        frf.load_fr_filter_and_write_baseline_list(datafile_list=[input_file], res_outfilename=outfilename,
+                                                     tol=1e-4, baseline_list=[bl[:2]],
+                                                     clobber=True, mode='dayenu',
+                                                     external_flags=flagfile, overwrite_flags=True,
+                                                     flag_yaml=flag_yaml)
+        # test that all flags are af yaml flags
+        hd = io.HERAData(outfilename)
+        d, f, n = hd.read()
+        for k in f:
+            assert np.all(f[k][:, 0])
+            assert np.all(f[k][:, 1])
+            assert np.all(f[k][:, 10:20])
+            assert np.all(f[k][:, 60])
+        os.remove(outfilename)
+        shutil.rmtree(cdir)
+
+    def test_load_fr_filter_and_write(self, tmpdir):
+        tmp_path = tmpdir.strpath
+        uvh5 = os.path.join(DATA_PATH, "test_input/zen.2458101.46106.xx.HH.OCR_53x_54x_only.uvh5")
+        outfilename = os.path.join(tmp_path, 'temp.h5')
+        frf.load_fr_filter_and_write(uvh5, res_outfilename=outfilename, tol=1e-4, clobber=True, Nbls_per_load=1)
+        hd = io.HERAData(outfilename)
+        d, f, n = hd.read(bls=[(53, 54, 'ee')])
+        for bl in d:
+            assert not np.all(np.isclose(d[bl], 0.))
+
+        xfil = xf.XTalkFilter(uvh5, filetype='uvh5')
+        xfil.read(bls=[(53, 54, 'ee')])
+        xfil.run_fr_filter(to_filter=[(53, 54, 'ee')], tol=1e-4, verbose=True)
+        np.testing.assert_almost_equal(d[(53, 54, 'ee')], xfil.clean_resid[(53, 54, 'ee')], decimal=5)
+        np.testing.assert_array_equal(f[(53, 54, 'ee')], xfil.flags[(53, 54, 'ee')])
+
+        # test loading and writing all baselines at once.
+        uvh5 = os.path.join(DATA_PATH, "test_input/zen.2458101.46106.xx.HH.OCR_53x_54x_only.uvh5")
+        outfilename = os.path.join(tmp_path, 'temp.h5')
+        for avg_bl in [True, False]:
+            frf.load_fr_filter_and_write(uvh5, res_outfilename=outfilename, tol=1e-4, clobber=True,
+                                           Nbls_per_load=None, avg_red_bllens=avg_bl)
+            hd = io.HERAData(outfilename)
+            d, f, n = hd.read(bls=[(53, 54, 'ee')])
+            for bl in d:
+                assert not np.all(np.isclose(d[bl], 0.))
+
+        frfil = frf.FRFilter(uvh5, filetype='uvh5')
+        frfil.read(bls=[(53, 54, 'ee')])
+        frfil.run_fr_filter(to_filter=[(53, 54, 'ee')], tol=1e-4, verbose=True)
+        np.testing.assert_almost_equal(d[(53, 54, 'ee')], xfil.clean_resid[(53, 54, 'ee')], decimal=5)
+        np.testing.assert_array_equal(f[(53, 54, 'ee')], xfil.flags[(53, 54, 'ee')])
+
+        cal = os.path.join(DATA_PATH, "test_input/zen.2458101.46106.xx.HH.uv.abs.calfits_54x_only")
+        outfilename = os.path.join(tmp_path, 'temp.h5')
+        os.remove(outfilename)
+        for avg_bl in [True, False]:
+            frf.load_fr_filter_and_write(uvh5, calfile=cal, tol=1e-4, res_outfilename=outfilename,
+                                           Nbls_per_load=2, clobber=True, avg_red_bllens=avg_bl)
+            hd = io.HERAData(outfilename)
+            assert 'Thisfilewasproducedbythefunction' in hd.history.replace('\n', '').replace(' ', '')
+            d, f, n = hd.read()
+            for bl in d:
+                if not np.all(f[bl]):
+                    assert not np.all(np.isclose(d[bl], 0.))
+            np.testing.assert_array_equal(f[(53, 54, 'ee')], True)
+            os.remove(outfilename)
+
+        # prepare an input file for broadcasting flags
+        input_file = os.path.join(tmp_path, 'temp_special_flags.h5')
+        shutil.copy(uvh5, input_file)
+        hd = io.HERAData(input_file)
+        _, flags, _ = hd.read()
+        ntimes_before = hd.Ntimes
+        nfreqs_before = hd.Nfreqs
+        freqs_before = hd.freqs
+        times_before = hd.times
+        for bl in flags:
+            flags[bl][:] = False
+            flags[bl][0, :hd.Nfreqs // 2] = True  # first time has 50% flagged
+            flags[bl][-3:, -1] = True  # last channel has flags for three integrations
+        hd.update(flags=flags)
+        hd.write_uvh5(input_file, clobber=True)
+        # this time_threshold will result in
+        # entire first integration begin flagged
+        # and entire final channel being flagged
+        # when flags are broadcasted.
+        time_thresh = 2. / hd.Ntimes
+        frf.load_fr_filter_and_write(input_file, res_outfilename=outfilename, tol=1e-4,
+                                       factorize_flags=True, time_thresh=time_thresh, clobber=True)
+        hd = io.HERAData(outfilename)
+        d, f, n = hd.read(bls=[(53, 54, 'ee')])
+        for bl in f:
+            assert np.any(f[bl][:, :-1])
+            assert np.all(f[bl][0, :])
+
+        # test delay filtering and writing with factorized flags and partial i/o
+        frf.load_fr_filter_and_write(input_file, res_outfilename=outfilename, tol=1e-4,
+                                       factorize_flags=True, time_thresh=time_thresh, clobber=True)
+        hd = io.HERAData(outfilename)
+        d, f, n = hd.read(bls=[(53, 54, 'ee')])
+        for bl in f:
+            # check that flags were broadcasted.
+            assert np.all(f[bl][0, :])
+            assert np.all(f[bl][:, -1])
+            assert not np.all(np.isclose(d[bl], 0.))
+
+        frf.load_fr_filter_and_write(input_file, res_outfilename=outfilename, tol=1e-4, Nbls_per_load=1,
+                                       factorize_flags=True, time_thresh=time_thresh, clobber=True)
+        hd = io.HERAData(outfilename)
+        d, f, n = hd.read(bls=[(53, 54, 'ee')])
+        for bl in f:
+            # check that flags were broadcasted.
+            assert np.all(f[bl][0, :])
+            assert np.all(f[bl][:, -1])
+            assert not np.all(np.isclose(d[bl], 0.))
+
+        # test apriori flags and flag_yaml
+        hd = io.HERAData(uvh5)
+        hd.read()
+        flag_yaml = os.path.join(DATA_PATH, 'test_input/a_priori_flags_sample.yaml')
+        uvf = UVFlag(hd, mode='flag', copy_flags=True)
+        uvf.to_waterfall(keep_pol=False, method='and')
+        uvf.flag_array[:] = False
+        flagfile = os.path.join(tmp_path, 'test_flag.h5')
+        uvf.write(flagfile, clobber=True)
+        frf.load_fr_filter_and_write(uvh5, res_outfilename=outfilename,
+                                       Nbls_per_load=1, clobber=True, mode='dpss_leastsq',
+                                       external_flags=flagfile,
+                                       overwrite_flags=True)
+        # test that all flags are False
+        hd = io.HERAData(outfilename)
+        d, f, n = hd.read(bls=[(53, 54, 'ee')])
+        for k in f:
+            assert np.all(~f[k])
+        # now without parital io.
+        frf.load_fr_filter_and_write(uvh5, res_outfilename=outfilename,
+                                       clobber=True, mode='dpss_leastsq',
+                                       external_flags=flagfile,
+                                       overwrite_flags=True)
+        # test that all flags are False
+        hd = io.HERAData(outfilename)
+        d, f, n = hd.read(bls=[(53, 54, 'ee')])
+        for k in f:
+            assert np.all(~f[k])
+
+
+    def test_frf_clean_argparser(self):
+        sys.argv = [sys.argv[0], 'a', '--clobber', '--window', 'blackmanharris']
+        parser = frf.fr_filter_argparser()
+        a = parser.parse_args()
+        assert a.infilename == 'a'
+        assert a.clobber is True
+        assert a.window == 'blackmanharris'
+        assert a.time_thresh == 0.05
+        assert not a.factorize_flags
+        assert a.frate_standoff == 0.0
+        assert a.frac_frate_sky_max == 1.0
+        assert a.min_frate == 0.0
+
+    def test_frf_linear_argparser(self):
+        sys.argv = [sys.argv[0], 'a', '--clobber', '--write_cache', '--cache_dir', '/blah/', '--min_frate', '0.1', '--frate_standoff', '3.2', '--frac_frate_sky_max', '.99']
+        parser = frf.fr_filter_argparser(mode='dayenu')
+        a = parser.parse_args()
+        assert a.infilename == 'a'
+        assert a.clobber is True
+        assert a.write_cache is True
+        assert a.cache_dir == '/blah/'
+        assert a.time_thresh == 0.05
+        assert a.frate_standoff == 3.2
+        assert a.frac_frate_sky_max == .99
+        assert a.min_frate == 0.1
+        assert not a.factorize_flags
+        parser = frf.fr_filter_argparser(mode='dpss_leastsq')
+        a = parser.parse_args()
+        assert a.infilename == 'a'
+        assert a.clobber is True
+        assert a.write_cache is True
+        assert a.cache_dir == '/blah/'
+        assert a.time_thresh == 0.05
+        assert not a.factorize_flags
+        assert a.frate_standoff == 3.2
+        assert a.frac_frate_sky_max == .99
+        assert a.min_frate == 0.1
