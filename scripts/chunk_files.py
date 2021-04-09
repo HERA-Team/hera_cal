3cc523524108d9782d8e7e2fae0193f8c7a70569
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-04-08 19:44:37 -0700

    update setup.py and bash script.

--- scripts/chunk_files.py
+++ scripts/chunk_files.py
@@ -11,7 +11,7 @@ from hera_cal import chunker
 ap = chunker.chunk_parser()
 args = ap.parse_args()
 
-chunker.chunk_data_files(filenames=args.filenames, outputfile=args.outputfile,
+chunker.chunk_files(filenames=args.filenames, outputfile=args.outputfile,
                          chunk_size=args.chunk_size, clobber=args.clobber, ant_flag_yaml=args.ant_flag_yaml,
                          inputfile=args.inputfile, type=args.type, polarizations=args.polarizations,
                          spw_range=args.spw_range, throw_away_flagged_ants=args.throw_away_flagged_ants)
