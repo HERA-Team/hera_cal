14caa99c12c16282d114294c4d89329830eb128e
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-04-13 09:56:49 -0700

    update scripts.

--- scripts/delay_filter_run.py
+++ scripts/delay_filter_run.py
@@ -12,24 +12,24 @@ parser = delay_filter.delay_filter_argparser()
 a = parser.parse_args()
 
 # set kwargs
-if a.mode == 'clean':
-    filter_kwargs = {'window': a.window, 
-                     'maxiter': a.maxiter, 'edgecut_hi': a.edgecut_hi,
-                     'edgecut_low': a.edgecut_low, 'min_dly': a.min_dly, 'gain': a.gain}
-    if a.window == 'tukey':
-        filter_kwargs['alpha'] = a.alpha
+if ap.mode == 'clean':
+    filter_kwargs = {'window': ap.window,
+                     'maxiter': ap.maxiter, 'edgecut_hi': ap.edgecut_hi,
+                     'edgecut_low': ap.edgecut_low, 'min_dly': ap.min_dly, 'gain': ap.gain}
+    if ap.window == 'tukey':
+        filter_kwargs['alpha'] = ap.alpha
     avg_red_bllens = False
     skip_gaps_larger_then_filter_period = False
     skip_flagged_edges = False
     max_contiguous_edge_flags = 10000
     flag_model_rms_outliers = False
-elif a.mode == 'dayenu':
+elif ap.mode == 'dayenu':
     filter_kwargs = {}
     avg_red_bllens = True
     skip_gaps_larger_then_filter_period = False
     max_contiguous_edge_flags = 10000
     flag_model_rms_outliers = False
-elif a.mode == 'dpss_leastsq':
+elif ap.mode == 'dpss_leastsq':
     filter_kwargs = {}
     avg_red_bllens = True
     skip_gaps_larger_then_filter_period = True
@@ -38,32 +38,31 @@ elif a.mode == 'dpss_leastsq':
     flag_model_rms_outliers = True
 else:
     raise ValueError(f"mode {mode} not supported.")
-filter_kwargs['zeropad'] = a.zeropad
 
 if args.cornerturnfile is not None:
-    baseline_list = io.baselines_from_filelist_position(filename=a.cornerturnfile, filelist=a.datafilelist)
+    baseline_list = io.baselines_from_filelist_position(filename=ap.cornerturnfile, filelist=ap.datafilelist)
 else:
     baseline_list = None
 
-# allow none string to be passed through to a.calfile
-if isinstance(a.calfile_list, str) and a.calfile_list.lower() == 'none':
-    a.calfile_list = None
+# allow none string to be passed through to ap.calfile
+if isinstance(ap.calfilelist, str) and ap.calfilelist.lower() == 'none':
+    ap.calfilelist = None
 # Run Delay Filter
-delay_filter.load_delay_filter_and_write(a.datafilelist, calfile_list=a.calfilelist, avg_red_bllens=avg_red_bllens,
-                                         baseline_list=baseline_list, spw_range=a.spw_range,
-                                         cache_dir=a.cache_dir, res_outfilename=a.res_outfilename,
-                                         clobber=a.clobber, write_cache=a.write_cache, external_flags=a.external_flags,
-                                         read_cache=a.read_cache, mode=a.mode, overwrite_flags=a.overwrite_flags,
-                                         factorize_flags=a.factorize_flags, time_thresh=a.time_thresh,
+delay_filter.load_delay_filter_and_write(ap.datafilelist, calfile_list=ap.calfilelist, avg_red_bllens=avg_red_bllens,
+                                         baseline_list=baseline_list, spw_range=ap.spw_range,
+                                         cache_dir=ap.cache_dir, res_outfilename=ap.res_outfilename,
+                                         clobber=ap.clobber, write_cache=ap.write_cache, external_flags=ap.external_flags,
+                                         read_cache=ap.read_cache, mode=ap.mode, overwrite_flags=ap.overwrite_flags,
+                                         factorize_flags=ap.factorize_flags, time_thresh=ap.time_thresh,
                                          max_contiguous_edge_flags=max_contiguous_edge_flags,
-                                         add_to_history=' '.join(sys.argv), polarizations=a.polarizations,
-                                         verbose=a.verbose, skip_if_flag_within_edge_distance=a.skip_if_flag_within_edge_distance,
-                                         flag_yaml=a.flag_yaml, Nbls_per_load=a.Nbls_per_load,
+                                         add_to_history=' '.join(sys.argv), polarizations=ap.polarizations,
+                                         verbose=ap.verbose, skip_if_flag_within_edge_distance=ap.skip_if_flag_within_edge_distance,
+                                         flag_yaml=ap.flag_yaml, Nbls_per_load=ap.Nbls_per_load,
                                          skip_contiguous_flags=skip_gaps_larger_then_filter_period,
                                          skip_flagged_edges=skip_flagged_edges,
-                                         filled_outfilename=a.filled_outfilename,
-                                         CLEAN_outfilename=a.CLEAN_outfilename,
-                                         standoff=a.standoff, horizon=a.horizon, tol=a.tol,
-                                         skip_wgt=a.skip_wgt, min_dly=a.min_dly,
+                                         filled_outfilename=ap.filled_outfilename,
+                                         CLEAN_outfilename=ap.CLEAN_outfilename,
+                                         standoff=ap.standoff, horizon=ap.horizon, tol=ap.tol,
+                                         skip_wgt=ap.skip_wgt, min_dly=ap.min_dly, zeropad=ap.zeropad,
                                          flag_model_rms_outliers=flag_model_rms_outliers,
                                          clean_flags_in_resid_flags=True, **filter_kwargs)
