38603d59ef74a5341f34c1312024e03559586c17
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-04-12 23:25:08 -0700

    fix delay parser.

--- hera_cal/vis_clean.py
+++ hera_cal/vis_clean.py
@@ -1676,7 +1676,6 @@ def _filter_argparser():
     a.add_argument("datafilelist", default=None, type=str, nargs="+", help="list of data files to read in and perform filtering on.")
     a.add_argument("--filetype_in", type=str, default='uvh5', help='filetype of input data files (default "uvh5")')
     a.add_argument("--filetype_out", type=str, default='uvh5', help='filetype for output data files (default "uvh5")')
-    a.add_argument("--res_outfilename", default=None, type=str, help="path for writing the filtered visibilities with flags")
     a.add_argument("--clobber", default=False, action="store_true", help='overwrites existing file at outfile')
     a.add_argument("--spw_range", type=int, default=None, nargs=2, help="spectral window of data to foreground filter.")
     a.add_argument("--tol", type=float, default=1e-9, help='Threshold for foreground and xtalk subtraction (default 1e-9)')
@@ -1689,30 +1688,10 @@ def _filter_argparser():
     a.add_argument("--factorize_flags", default=False, action="store_true", help="Factorize flags.")
     a.add_argument("--time_thresh", type=float, default=0.05, help="time threshold above which to completely flag channels and below which to flag times with flagged channel.")
     a.add_argument("--calfilelist", default=None, type=str, nargs="+", help="list of calibration files.")
-    return a
-
-
-# ------------------------------------------
-# Here are arg-parsers clean filtering
-# for both xtalk and foregrounds.
-# ------------------------------------------
-
-
-def _clean_argparser():
-    '''
-    Arg parser for commandline operation of hera_cal.delay_filter in various clean modes.
-    Arguments
-    ---------
-        None
-
-    Returns
-    -------
-        Arg-parser for linear filtering. Still needs domain specific args (delay versus xtalk).
-    '''
-    a = _filter_argparser()
     a.add_argument("--CLEAN_outfilename", default=None, type=str, help="path for writing the filtered model visibilities (with the same flags)")
     a.add_argument("--filled_outfilename", default=None, type=str, help="path for writing the original data but with flags unflagged and replaced with filtered models wherever possible")
-    clean_options = a.add_argument_group(title='Options for CLEAN')
+    a.add_argument("--res_outfilename", default=None, type=str, help="path for writing the filtered visibilities with flags")
+    clean_options = a.add_argument_group(title='Options for CLEAN (only used when mode == "clean")')
     clean_options.add_argument("--window", type=str, default='blackman-harris', help='window function for frequency filtering (default "blackman-harris",\
                               see uvtools.dspec.gen_window for options')
     clean_options.add_argument("--maxiter", type=int, default=100, help='maximum iterations for aipy.deconv.clean to converge (default 100)')
@@ -1720,49 +1699,13 @@ def _clean_argparser():
     clean_options.add_argument("--edgecut_hi", default=0, type=int, help="Number of channels to flag on upper band edge and exclude from window function.")
     clean_options.add_argument("--gain", type=float, default=0.1, help="Fraction of residual to use in each iteration.")
     clean_options.add_argument("--alpha", type=float, default=.5, help="If window='tukey', use this alpha parameter (default .5).")
+    cache_options = a.add_argument_group(title='Options for caching (only used when mode == "dayenu" or "dpss_leastsq")')
+    cache_options.add_argument("--write_cache", default=False, action="store_true", help="if True, writes newly computed filter matrices to cache.")
+    cache_options.add_argument("--cache_dir", type=str, default=None, help="directory to store cached filtering matrices in.")
+    cache_options.add_argument("--read_cache", default=False, action="store_true", help="If true, read in cache files in directory specified by cache_dir.")
+    cache_options.add_argument("--max_contiguous_edge_flags", type=int, default=1, help="Skip integrations with at least this number of contiguous edge flags.")
     return a
-
-# ------------------------------------------
-# Here are are parsers for linear filters.
-# for xtalk and foregrounds.
-# ------------------------------------------
-
-
-def _linear_argparser():
-    '''
-    Arg parser for commandline operation of hera_cal.delay_filter in various linear modes.
-    Arguments
-    ---------
-        None
-    Returns
-    -------
-        Arg-parser for linear filtering. Still needs domain specific args (delay versus xtalk)
-    '''
-    a = _filter_argparser()
-    cache_options = a.add_argument_group(title='Options for caching')
-    a.add_argument("--write_cache", default=False, action="store_true", help="if True, writes newly computed filter matrices to cache.")
-    a.add_argument("--cache_dir", type=str, default=None, help="directory to store cached filtering matrices in.")
-    a.add_argument("--read_cache", default=False, action="store_true", help="If true, read in cache files in directory specified by cache_dir.")
-    a.add_argument("--max_contiguous_edge_flags", type=int, default=1, help="Skip integrations with at least this number of contiguous edge flags.")
-    return a
-
-
-def _dpss_argparser():
-    '''
-    Arg parser for commandline operation of hera_cal.delay_filter in dpss mode.
-    Arguments
-    ---------
-        None
-    Returns
-    -------
-        Arg-parser for dpss filtering.
-    '''
-    a = _linear_argparser()
-    a.add_argument("--CLEAN_outfilename", default=None, type=str, help="path for writing the filtered model visibilities (with the same flags)")
-    a.add_argument("--filled_outfilename", default=None, type=str, help="path for writing the original data but with flags unflagged and replaced with filtered models wherever possible")
-    return a
-
-
+    
 def time_chunk_from_baseline_chunks(time_chunk_template, baseline_chunk_files, outfilename, clobber=False, time_bounds=False):
     """Combine multiple waterfall files (with disjoint baseline sets) into time-limited file with all baselines.
 
