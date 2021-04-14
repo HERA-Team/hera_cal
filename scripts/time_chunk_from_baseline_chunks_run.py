f851e729969734b5d8c8cc3ba2e4ef38126fd5d2
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-04-13 17:21:08 -0700

    fix clobber.

--- scripts/time_chunk_from_baseline_chunks_run.py
+++ scripts/time_chunk_from_baseline_chunks_run.py
@@ -12,5 +12,5 @@ parser = vis_clean.time_chunk_from_baseline_chunks()
 a = parser.parse_args()
 
 vis_clean.time_chunk_from_baseline_chunks(time_chunk_template=a.time_chunk_template,
-                                         baseline_chunk_files=a.baseline_chunk_files,
+                                         baseline_chunk_files=a.baseline_chunk_files, clobber=a.clobber,
                                          outfilename=a.outfilename, time_bounds=a.time_bounds)
