a092bb1c4cb11758a0a64ab669fc44641b031bdb
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-04-13 19:13:02 -0700

    remove a blank line.

--- hera_cal/frf.py
+++ hera_cal/frf.py
@@ -19,7 +19,6 @@ from . import io
 import warnings
 
 
-
 def timeavg_waterfall(data, Navg, flags=None, nsamples=None, wgt_by_nsample=True, rephase=False,
                       lsts=None, freqs=None, bl_vec=None, lat=-30.72152, extra_arrays={}, verbose=True):
     """
