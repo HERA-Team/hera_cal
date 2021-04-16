028d866bca241ed18dc0ab9b2d0f5e2d187f3f91
Josh Dillon <jsdillon@berkeley.edu>
2021-04-16 15:33:55 -0700

    fix docstrings

--- scripts/red_average.py
+++ scripts/red_average.py
@@ -10,8 +10,8 @@ import argparse
 from hera_cal import io, utils, redcal
 
 # Parse arguments
-a = argparse.ArgumentParser(description="Apply (and optionally, also unapply) a calfits file to visibility file.")
-a.add_argument("infilename", type=str, help="path to visibility data file to calibrate") 
+a = argparse.ArgumentParser(description="Redundantly average a data file that's already been calibrated.")
+a.add_argument("infilename", type=str, help="path to visibility data file to redundantly average") 
 a.add_argument("outfilename", type=str, help="path to new visibility results file")
 a.add_argument("--bl_error_tol", type=float, default=1.0, help="Baseline redundancy tolerance in meters.")
 a.add_argument("--clobber", default=False, action="store_true", help='overwrites existing file at outfile')
