3a3e438df150303c14be374e3be1eff15f6d5da8
Josh Dillon <jsdillon@berkeley.edu>
2021-04-16 16:27:48 -0700

    minor edits followed @aewallwi's review

--- scripts/red_average.py
+++ scripts/red_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2019 the HERA Project
+# Copyright 2021 the HERA Project
 # Licensed under the MIT License
 
 """Command-line script for redundant averaging of (calibrated) data. To calibrate and then average, try apply_cal.py"""
@@ -10,12 +10,12 @@ import argparse
 from hera_cal import io, utils, redcal
 
 # Parse arguments
-a = argparse.ArgumentParser(description="Redundantly average a data file that's already been calibrated.")
-a.add_argument("infilename", type=str, help="path to visibility data file to redundantly average") 
-a.add_argument("outfilename", type=str, help="path to new visibility results file")
-a.add_argument("--bl_error_tol", type=float, default=1.0, help="Baseline redundancy tolerance in meters.")
-a.add_argument("--clobber", default=False, action="store_true", help='overwrites existing file at outfile')
-args = a.parse_args()
+ap = argparse.ArgumentParser(description="Redundantly average a data file that's already been calibrated.")
+ap.add_argument("infilename", type=str, help="path to visibility data file to redundantly average") 
+ap.add_argument("outfilename", type=str, help="path to new visibility results file")
+ap.add_argument("--bl_error_tol", type=float, default=1.0, help="Baseline redundancy tolerance in meters.")
+ap.add_argument("--clobber", default=False, action="store_true", help='overwrites existing file at outfile')
+args = ap.parse_args()
 
 # Load data
 hd = io.HERAData(args.infilename)
