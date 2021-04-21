f248ba83ecee4c0b3e8c405b671691c2c13eebf2
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-04-20 22:13:32 -0700

    script to flag all values in a file and set data/nsamples to zero.

---
+++ scripts/flag_all.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2021 the HERA Project
+# Licensed under the MIT License
+
+"""Command-line script for completely flagging a data-file."""
+
+import sys
+import argparse
+from hera_cal import io, utils, redcal
+
+# Parse arguments
+ap = argparse.ArgumentParser(description="Completely Flag a data file.")
+ap.add_argument("infilename", type=str, help="path to visibility data to completely flag.")
+ap.add_argument("outfilename", type=str, help="path to new visibility file to write out completely flagged data")
+ap.add_argument("--clobber", default=False, action="store_true", help='overwrites existing file at outfile')
+ap.add_argument("--fill_data_with_zeros", default=False, action="store_true", help='Fills the data array with zeros.')
+ap.add_argument("--fill_nsamples_with_zeros", default=False, action="store_true", help='Fills the nsamples array with zeros.')
+
+args = ap.parse_args()
+
+# Load data
+hd = io.HERAData(args.infilename)
+hd.read()
+
+# completely flag.
+hd.flag_array[:] = True
+
+# fill data with zeros.
+if ap.fill_data_with_zeros:
+    hd.data_array[:] = 0.0 + 0.0j
+
+# fill nsamples with zeros.
+if ap.fill_nsamples_with_zeros:
+    hd.nsample_array[:] = 0
+
+# Write data
+hd.write_uvh5(args.outfilename, clobber=args.clobber)
