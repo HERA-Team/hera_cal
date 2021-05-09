445b41cd85a86a3031a4fb7ffafe5db5c72244e6
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-05-08 18:50:42 -0700

    remove r

--- scripts/tophat_frfilter_run.py
+++ scripts/tophat_frfilter_run.py
@@ -62,4 +62,4 @@ frf.load_tophat_frfilter_and_write(ap.datafilelist, calfile_list=ap.calfilelist,
                                    skip_if_flag_within_edge_distance=ap.skip_if_flag_within_edge_distance,
                                    zeropad=ap.zeropad, tol=ap.tol, skip_wgt=ap.skip_wgt, max_frate_coeffs=ap.max_frate_coeffs,
                                    frac_frate_sky_max=ap.frac_frate_sky_max, frate_standoff=ap.frate_standoff, min_frate=ap.min_frate,
-                                   clean_flags_in_resid_flags=apr.clean_flags_in_resid_flags, **filter_kwargs)
+                                   clean_flags_in_resid_flags=ap.clean_flags_in_resid_flags, **filter_kwargs)
