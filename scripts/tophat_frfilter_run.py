f48c203a3bf5683489139af3c5d868ba4fe92722
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-09-04 11:57:25 -0700

    pass mainlobe arg

--- scripts/tophat_frfilter_run.py
+++ scripts/tophat_frfilter_run.py
@@ -61,4 +61,5 @@ frf.load_tophat_frfilter_and_write(ap.datafilelist, calfile_list=ap.calfilelist,
                                    skip_if_flag_within_edge_distance=ap.skip_if_flag_within_edge_distance,
                                    zeropad=ap.zeropad, tol=ap.tol, skip_wgt=ap.skip_wgt, max_frate_coeffs=ap.max_frate_coeffs,
                                    frac_frate_sky_max=ap.frac_frate_sky_max, frate_standoff=ap.frate_standoff, min_frate=ap.min_frate,
+                                   mainlobe_radius=ap.mainlobe_radius,
                                    clean_flags_in_resid_flags=True, select_mainlobe=ap.select_mainlobe, mainlobe_width=ap.mainlobe_width, **filter_kwargs)
