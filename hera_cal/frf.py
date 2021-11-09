aa7b2aa9a3fb6a43d1c683133f11ace8fcfb8fa5
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-11-09 12:56:19 -0800

    change width -> half-width in docstrings.

--- hera_cal/frf.py
+++ hera_cal/frf.py
@@ -43,7 +43,7 @@ def sky_frates(uvd, keys=None, frate_standoff=0.0, frate_width_multiplier=1.0, m
         fraction of width of range of fringe-rates associated with sky emission to filter.
         default is 1.0
     min_frate_half_width: float, optional
-        minimum width of fringe-rate filter, regardless of baseline length in mHz.
+        minimum half-width of fringe-rate filter, regardless of baseline length in mHz.
         Serves the same function as "min_dly" in delay filters.
         Default is 0.025
 
@@ -602,7 +602,7 @@ class FRFilter(VisClean):
          fraction of horizon to fringe-rate filter.
          default is 1.0
         min_frate_half_width: float, optional
-            minimum width of fringe-rate filter, regardless of baseline length in mHz.
+            minimum half-width of fringe-rate filter, regardless of baseline length in mHz.
             Serves the same function as "min_dly" in delay filters.
             Default is 0.025
         max_frate_coeffs, 2-tuple float
@@ -746,7 +746,7 @@ def tophat_frfilter_argparser(mode='clean'):
                                                                               "Used if select_mainlobe is False and max_frate_coeffs not specified.")
     ap.add_argument("--frate_standoff", type=float, default=0.0, help="Standoff in fringe-rate to filter [mHz]."
                                                                       "Used of select_mainlobe is False and max_frate_coeffs not specified.")
-    ap.add_argument("--min_frate_half_width", type=float, default=0.025, help="minimum width of fringe-rate filter, regardless of baseline length in mHz."
+    ap.add_argument("--min_frate_half_width", type=float, default=0.025, help="minimum half-width of fringe-rate filter, regardless of baseline length in mHz."
                                                                               "Serves the same function as "min_dly" in delay filters."
                                                                               "Default is 0.025.")
     ap.add_argument("--max_frate_coeffs", type=float, default=None, nargs=2, help="Maximum fringe-rate coefficients for the model max_frate [mHz] = x1 * EW_bl_len [ m ] + x2."
