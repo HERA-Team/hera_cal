7ab51e68b1add012edfecaa6cd0b6000f63c3e8d
Josh Dillon <jsdillon@berkeley.edu>
2022-01-27 18:45:42 -0800

    handle case where cal_flags is None better

--- hera_cal/apply_cal.py
+++ hera_cal/apply_cal.py
@@ -264,20 +264,24 @@ def calibrate_in_place(data, new_gains, data_flags=None, cal_flags=None, old_gai
                     flag_all = True
 
         if data_flags is not None:
-            # update data_flags in the case where flags are weights, flag all if cal_flags are missing
-            if flags_are_wgts:
-                try:
-                    data_flags[(i, j, pol)] *= (~cal_flags_here[(i, ap1)]).astype(np.float)
-                    data_flags[(i, j, pol)] *= (~cal_flags_here[(j, ap2)]).astype(np.float)
-                except KeyError:
-                    flag_all = True
-            # update data_flags in the case where flags are booleans, flag all if cal_flags are missing
+            if cal_flags is None:
+                # when data_flags is provided but cal_flags is not, flag everything
+                flag_all = True
             else:
-                try:
-                    data_flags[(i, j, pol)] += cal_flags_here[(i, ap1)]
-                    data_flags[(i, j, pol)] += cal_flags_here[(j, ap2)]
-                except KeyError:
-                    flag_all = True
+                # update data_flags in the case where flags are weights, flag all if cal_flags are missing
+                if flags_are_wgts:
+                    try:
+                        data_flags[(i, j, pol)] *= (~cal_flags_here[(i, ap1)]).astype(np.float)
+                        data_flags[(i, j, pol)] *= (~cal_flags_here[(j, ap2)]).astype(np.float)
+                    except KeyError:
+                        flag_all = True
+                # update data_flags in the case where flags are booleans, flag all if cal_flags are missing
+                else:
+                    try:
+                        data_flags[(i, j, pol)] += cal_flags_here[(i, ap1)]
+                        data_flags[(i, j, pol)] += cal_flags_here[(j, ap2)]
+                    except KeyError:
+                        flag_all = True
 
             # if the flag object is given, update it for this baseline to be totally flagged
             if flag_all:
