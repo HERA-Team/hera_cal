fb9a2d6484962009b512849d53fcc6f4138799bc
Aaron Ewall-Wice <aaronew@berkeley.edu>
2022-04-19 01:36:45 -0700

    fix mode.

--- hera_cal/smooth_cal.py
+++ hera_cal/smooth_cal.py
@@ -163,6 +163,8 @@ def freq_filter(gains, wgts, freqs, filter_scale=10.0, skip_wgt=0.1,
     filter_size = (filter_scale * 1e6)**-1  # Puts it in s
     dly = single_iterative_fft_dly(gains, wgts, freqs)  # dly in s
     rephasor = np.exp(-2.0j * np.pi * dly * freqs)
+    if mode == 'DPSS':
+        mode = 'dpss_leastsq'
 
     filtered, res, info = uvtools.dspec.fourier_filter(x=freqs, data=gains * rephasor, wgts=wgts, mode=mode, filter_centers=[0.],
                                                        skip_wgt=skip_wgt, filter_half_widths=[filter_size], **filter_kwargs)
