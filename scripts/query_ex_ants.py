2f7065e802713386c6e6088bc12e4ecc044b0478
Josh Dillon <jsdillon@berkeley.edu>
2022-05-20 16:30:56 -0700

    Revert "local tmp copy of apriori for query_ex_ants"
    This reverts commit a033e37950a1076c22e832984d10974d225d4c45.

--- scripts/query_ex_ants.py
+++ scripts/query_ex_ants.py
@@ -9,40 +9,6 @@ import argparse
 from hera_mc import cm_active
 
 
-def get_active_apriori(at_date, float_format, window=10.0):
-    if float_format != 'jd':
-        raise ValueError('Just using for jd -- maybe later...')
-    make_new = False
-    aap = {}
-    try:
-        with open('active_apriori_working.tmp', 'r') as fp:
-            timestamp = float(fp.readline().strip().split()[1])
-            valid_statuses = fp.readline().strip().split(',')
-            if abs(at_date - timestamp) > window / 24.0 / 60.0:
-                make_new = True
-            else:
-                for line in fp:
-                    if not len(line.strip()):  # just in case an extra blank...
-                        continue
-                    ant, status = [x.strip() for x in line.split()]
-                    aap[ant] = status
-    except FileNotFoundError:
-        make_new = True
-
-    if make_new:
-        naap = cm_active.get_active(at_date=at_date, float_format=float_format)
-        valid_statuses = list(naap.apriori.values())[0].valid_statuses() +\
-            list(naap.apriori.values())[0].old_statuses()
-        with open('active_apriori_working.tmp', 'w') as fp:
-            print(at_date, file=fp)
-            print(','.join(valid_statuses), file=fp)
-            for ant in naap.apriori:
-                aap[ant] = naap.apriori[ant].status
-                print(f"{ant} {aap[ant]}", file=fp)
-
-    return aap, valid_statuses
-
-
 def query_ex_ants(JD, good_statuses):
     '''Query the HERA CM database for antennas considered bad on a certain date.
 
@@ -64,18 +30,19 @@ def query_ex_ants(JD, good_statuses):
         ex_ants: string of space-separated antennas considered bad
     '''
     # Load database
-    h, valid_statuses = get_active_apriori(at_date=JD, float_format='jd')
+    h = cm_active.get_active(at_date=JD, float_format='jd')
 
     # Check that input statuses are sensible
     good_statuses = [status.strip() for status in good_statuses.split(',')]
     assert len(good_statuses) > 0, 'There must be at least one input good status.'
+    valid_statuses = list(h.apriori.values())[0].valid_statuses() + list(h.apriori.values())[0].old_statuses()
     for status in good_statuses:
         assert status in valid_statuses, 'Invalid Status: {}'.format(status)
 
     # Pick out antnenna names with bad statuses
     exants = []
-    for ant, status in h.items():
-        if status not in good_statuses:
+    for ant in h.apriori:
+        if h.apriori[ant].status not in good_statuses:
             exants.append(int(ant[2:].split(':')[0]))  # Assumes the format HH0:A or HA330:A
 
     # Return sorted exants
