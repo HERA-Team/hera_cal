aa5b3aa356c9f6dbafcceab596871944aa3dfd37
Aaron Ewall-Wice <aaronew@berkeley.edu>
2022-06-05 21:22:00 -0700

    fix script names.

--- scripts/run_update_redcal_phase_degeneracy.py
+++ scripts/run_update_redcal_phase_degeneracy.py
@@ -3,7 +3,7 @@
 # Copyright 2022 the HERA Project
 # Licensed under the MIT License
 
-from hera_cal.redcal import nightly_median_firstcal_delays, nightly_median_firstcal_delays_argparser
+from hera_cal.redcal import update_redcal_phase_degeneracy, update_redcal_phase_degeneracy_argparser
 
 ap = update_redcal_phase_degeneracy_argparser()
 argvars = vars(ap.parse_args())
