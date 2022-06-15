648ef660da6a5f65f34ffc75cead3322e6330d27
Aaron Ewall-Wice <aaronew@berkeley.edu>
2022-06-15 18:44:06 +0800

    get rid of bad version line.

--- hera_cal/io.py
+++ hera_cal/io.py
@@ -21,7 +21,6 @@ import random
 import glob
 from pyuvdata.utils import POL_STR2NUM_DICT, POL_NUM2STR_DICT, ENU_from_ECEF, XYZ_from_LatLonAlt
 import argparse
-from . import version
 from hera_filters.dspec import place_data_on_uniform_grid
 
 
