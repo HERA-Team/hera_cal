12eefc6b6eff44f136ebf8c7b35e10fb13bc227c
Tyler Cox <tyler.a.cox@berkeley.edu>
2022-09-20 09:48:58 -0700

    Add remaining nucal utils

--- hera_cal/nucal.py
+++ hera_cal/nucal.py
@@ -9,121 +9,6 @@ from scipy.cluster.hierarchy import fclusterdata
 
 SPEED_OF_LIGHT = const.c.si.value
 
-class RadialRedundantGroup:
-    """
-    """
-    def __init__(self, baselines, antpos, blvec=None, pol=None):
-        """
-        """
-        _baselines = deepcopy(baselines)
-
-        # Attach polarization and normalized vector to radial redundant group
-        if pol is None:
-            pols = list(set([bl[2] for bl in baselines]))
-            if len(pols) > 1:
-                raise ValueError(
-                    f"Multiple polarizations are in your radially redundant group: {pols}"
-                )
-            else:
-                self.pol = pols[0]
-        else:
-            self.pol = pol
-            
-        if blvec is None:
-            ant1, ant2, pol = baselines[0]
-            self.blvec = (antpos[ant2] - antpos[ant1]) / np.linalg.norm(antpos[ant2] - antpos[ant1])
-        else:
-            self.blvec = blvec
-        
-        # Store baseline lengths
-        baseline_lengths = []
-        for baseline in baselines:
-            ant1, ant2, pol = baseline
-            baseline_lengths.append(np.linalg.norm(antpos[ant2] - antpos[ant1]))
-
-        # Sort baselines list by baseline length
-        self._baselines = [_baselines[idx] for idx in np.argsort(baseline_lengths)]
-        self.baseline_lengths = [
-            baseline_lengths[idx] for idx in np.argsort(baseline_lengths)
-        ]
-
-    def get_u_bounds(self, freqs):
-        """
-        Calculates the magnitude of the minimum and maximum u-modes values of the radial redundant group 
-        given an array of frequency values
-
-        Parameters:
-        ----------
-            freqs: np.ndarray
-                Array of frequencies found in the data in units of Hz
-
-        Returns:
-            ubounds: tuple
-                Tuple of the magnitude minimum and maximum u-modes sampled by this baseline group
-        """
-        umin = freqs.min() / 2.998e8 * np.min(self.baseline_lengths)
-        umax = freqs.max() / 2.998e8 * np.max(self.baseline_lengths)
-        return (umin, umax)
-
-    def filter_group(
-        self,
-        bls=None,
-        ex_bls=None,
-        ants=None,
-        ex_ants=None,
-        ubls=None,
-        ex_ubls=None,
-        pols=None,
-        ex_pols=None,
-        antpos=None,
-        min_bl_cut=None,
-        max_bl_cut=None,
-    ):
-        """
-        """
-        _baselines = redcal.filter_reds(
-            [self._baselines],
-            bls=bls,
-            ex_bls=ex_bls,
-            ants=ants,
-            ex_ants=ex_ants,
-            ubls=ubls,
-            ex_ubls=ex_ubls,
-            pols=pols,
-            ex_pols=ex_pols,
-        )
-        if len(_baselines) == 0:
-            self._baselines = []
-            self.baseline_lengths = []
-        else:
-            new_bls = []
-            new_bls_lengths = []
-            for bls in _baselines[0]:
-                index = self._baselines.index(bls)
-                if min_bl_cut is not None and self.baseline_lengths[index] < min_bl_cut:
-                    continue
-                if max_bl_cut is not None and self.baseline_lengths[index] > max_bl_cut:
-                    continue
-                new_bls.append(bls)
-                new_bls_lengths.append(self.baseline_lengths[index])
-
-            self._baselines = new_bls
-            self.baseline_lengths = new_bls_lengths
-
-    def __iter__(self):
-        """Iterate through baselines in the radially redundant group
-        """
-        return iter(self._baselines)
-
-    def __len__(self):
-        """Return the length of the baselines list
-        """
-        return len(self._baselines)
-
-    def __getitem__(self, index):
-        """Get the baseline at the chosen index
-        """
-        return self._baselines[index]
 
 def is_frequency_redundant(bl1, bl2, freqs, antpos, blvec_error_tol=1e-9):
     """
@@ -133,15 +18,16 @@ def is_frequency_redundant(bl1, bl2, freqs, antpos, blvec_error_tol=1e-9):
     Parameters:
     ----------
     bl1 : tuple
-        pass
+        Tuple of antenna indices and polarizations of the first baseline
     bl2 : tuple
-        pass
+        Tuple of antenna indices and polarizations of the second baseline
     freqs : np.ndarray
-        pass
+        Array of frequencies found in the data in units of Hz
     antpos : dict
-        pass
+        Antenna positions in the form {ant_index: np.array([x,y,z])}.
     blvec_error_tol : float, default=1e-9
-        pass
+        Largest allowable euclidean distance a unit baseline vector can be away from an existing
+        cluster to be considered a unique orientation. See "fclusterdata" for more details.
 
     Returns:
         Boolean value determining whether or not the baselines are frequency
@@ -180,3 +66,329 @@ def is_frequency_redundant(bl1, bl2, freqs, antpos, blvec_error_tol=1e-9):
     cond4 = clusters[0] == clusters[1]
 
     return (cond1 or cond2) and cond3 and cond4
+
+
+def get_unique_orientations(
+    antpos,
+    reds=None,
+    pols=["nn"],
+    min_ubl_per_orient=1,
+    blvec_error_tol=1e-9,
+    bl_error_tol=1.0,
+):
+    """
+    Sort baselines into groups with the same radial heading. These groups of baselines are potentially
+    frequency redundant in a similar way to redcal.get_reds does. Returns a list of RadialRedundantGroup objects
+
+    Parameters:
+    ----------
+    antpos : dict
+        Antenna positions in the form {ant_index: np.array([x,y,z])}.
+    reds : list of lists
+        List of lists of spatially redundant baselines in the array. Can be found using redcal.get_reds
+    pols : list, default=['nn']
+        A list of polarizations e.g. ['nn', 'ne', 'en', 'ee']
+    min_ubl_per_orient : int, default=1
+        Minimum number of baselines per unique orientation
+    blvec_error_tol : float, default=1e-9
+        Largest allowable euclidean distance a unit baseline vector can be away from an existing
+        cluster to be considered a unique orientation. See "fclusterdata" for more details.
+    bl_error_tol: float, default=1.0
+        The largest allowable difference between baselines in a redundant group
+        (in the same units as antpos). Normally, this is up to 4x the largest antenna position error.
+
+    """
+    if reds is None:
+        reds = redcal.get_reds(antpos, pols=pols, bl_error_tol=bl_error_tol)
+    else:
+        reds = deepcopy(reds)
+
+    _uors = {}
+    for pol in pols:
+        ubl_pairs = [red[0] for red in reds if red[0][-1] == pol]
+
+        # Compute normalized baseline vectors
+        normalized_vecs = []
+        for bls in ubl_pairs:
+            ant1, ant2, pol = bls
+            normalized_vecs.append(
+                (antpos[ant2] - antpos[ant1])
+                / np.linalg.norm(antpos[ant2] - antpos[ant1])
+            )
+
+        # Cluster orientations
+        clusters = fclusterdata(normalized_vecs, blvec_error_tol, criterion="distance")
+        uors = [[] for i in range(np.max(clusters))]
+
+        for cluster, bl in zip(clusters, ubl_pairs):
+            uors[cluster - 1].append(bl)
+
+        uors = sorted(uors, key=len, reverse=True)
+
+        # Find clusters with headings anti-parallel to others
+        for group in uors:
+            ant1, ant2, pol = group[0]
+            vec = (antpos[ant2] - antpos[ant1]) / np.linalg.norm(
+                antpos[ant2] - antpos[ant1]
+            )
+            vec = np.array(vec / blvec_error_tol, dtype=int)
+            if tuple(-vec) + (pol,) in _uors:
+                _uors[tuple(-vec) + (pol,)] += [utils.reverse_bl(bls) for bls in group]
+            else:
+                _uors[tuple(vec) + (pol,)] = group
+
+    # Convert lists to RadialRedundantGroup objects
+    uors = [
+        RadialRedundantGroup(_uors[key], antpos)
+        for key in _uors
+        if len(_uors[key]) >= min_ubl_per_orient
+    ]
+    uors = sorted(uors, key=len, reverse=True)
+    return uors
+
+
+class RadialRedundantGroup:
+    """ """
+
+    def __init__(self, baselines, antpos, blvec=None, pol=None):
+        """ """
+        _baselines = deepcopy(baselines)
+
+        # Attach polarization and normalized vector to radial redundant group
+        if pol is None:
+            pols = list(set([bl[2] for bl in baselines]))
+            if len(pols) > 1:
+                raise ValueError(
+                    f"Multiple polarizations are in your radially redundant group: {pols}"
+                )
+            else:
+                self.pol = pols[0]
+        else:
+            self.pol = pol
+
+        if blvec is None:
+            ant1, ant2, pol = baselines[0]
+            self.blvec = (antpos[ant2] - antpos[ant1]) / np.linalg.norm(
+                antpos[ant2] - antpos[ant1]
+            )
+        else:
+            self.blvec = blvec
+
+        # Store baseline lengths
+        baseline_lengths = []
+        for baseline in baselines:
+            ant1, ant2, pol = baseline
+            baseline_lengths.append(np.linalg.norm(antpos[ant2] - antpos[ant1]))
+
+        # Sort baselines list by baseline length
+        self._baselines = [_baselines[idx] for idx in np.argsort(baseline_lengths)]
+        self.baseline_lengths = [
+            baseline_lengths[idx] for idx in np.argsort(baseline_lengths)
+        ]
+
+    def get_u_bounds(self, freqs):
+        """
+        Calculates the magnitude of the minimum and maximum u-modes values of the radial redundant group
+        given an array of frequency values
+
+        Parameters:
+        ----------
+            freqs: np.ndarray
+                Array of frequencies found in the data in units of Hz
+
+        Returns:
+            ubounds: tuple
+                Tuple of the magnitude minimum and maximum u-modes sampled by this baseline group
+        """
+        umin = freqs.min() / 2.998e8 * np.min(self.baseline_lengths)
+        umax = freqs.max() / 2.998e8 * np.max(self.baseline_lengths)
+        return (umin, umax)
+
+    def filter_group(
+        self,
+        bls=None,
+        ex_bls=None,
+        ants=None,
+        ex_ants=None,
+        ubls=None,
+        ex_ubls=None,
+        pols=None,
+        ex_pols=None,
+        antpos=None,
+        min_bl_cut=None,
+        max_bl_cut=None,
+    ):
+        """ """
+        _baselines = redcal.filter_reds(
+            [self._baselines],
+            bls=bls,
+            ex_bls=ex_bls,
+            ants=ants,
+            ex_ants=ex_ants,
+            ubls=ubls,
+            ex_ubls=ex_ubls,
+            pols=pols,
+            ex_pols=ex_pols,
+        )
+        if len(_baselines) == 0:
+            self._baselines = []
+            self.baseline_lengths = []
+        else:
+            new_bls = []
+            new_bls_lengths = []
+            for bls in _baselines[0]:
+                index = self._baselines.index(bls)
+                if min_bl_cut is not None and self.baseline_lengths[index] < min_bl_cut:
+                    continue
+                if max_bl_cut is not None and self.baseline_lengths[index] > max_bl_cut:
+                    continue
+                new_bls.append(bls)
+                new_bls_lengths.append(self.baseline_lengths[index])
+
+            self._baselines = new_bls
+            self.baseline_lengths = new_bls_lengths
+
+    def __iter__(self):
+        """Iterate through baselines in the radially redundant group"""
+        return iter(self._baselines)
+
+    def __len__(self):
+        """Return the length of the baselines list"""
+        return len(self._baselines)
+
+    def __getitem__(self, index):
+        """Get the baseline at the chosen index"""
+        return self._baselines[index]
+
+
+class FrequencyRedundancy:
+    """ """
+
+    def __init__(
+        self, antpos, reds=None, blvec_error_tol=1e-9, pols=["nn"], bl_error_tol=1.0
+    ):
+        """
+        Parameters:
+        ----------
+        antpos : dict
+            Antenna positions in the form {ant_index: np.array([x,y,z])}.
+        reds : list of list
+            List of lists of baseline keys. Can be determined using redcal.get_reds
+        pols : list of strs
+            List of polarization strings to be used in the frequency redundant group
+        """
+        self.antpos = antpos
+
+        if reds is None:
+            reds = redcal.get_reds(antpos, pols=pols, bl_error_tol=bl_error_tol)
+
+        self._mapped_reds = {red[0]: red for red in reds}
+
+        self._radial_groups = get_unique_orientations(
+            antpos, reds=reds, pols=pols, blvec_error_tol=blvec_error_tol
+        )
+
+    def get_radial_group(self, key):
+        """
+        Get baselines with the same heading as a given baseline
+
+        Parameters:
+            key: tuple
+                Basleine key of type (ant1, ant2, pol)
+        """
+        # Identify headings
+        for group_key in self._mapped_reds:
+            if key == group_key or key in self._mapped_reds[group_key]:
+                key = group_key
+                break
+
+        for group in self._radial_groups:
+            if key in group:
+                return group
+
+    def get_redundant_group(self, key):
+        """
+        Get a list of baseline that are spatially redundant with the input baseline
+
+        Parameters:
+            key: tuple
+                Baseline key with of type (ant1, ant2, pol)
+        """
+        for group_key in self._mapped_reds:
+            if key == group_key or key in self._mapped_reds[group_key]:
+                key = group_key
+                break
+
+        if utils.reverse_bl(key) in self._mapped_reds:
+            return [
+                utils.reverse_bl(bls)
+                for bls in self._mapped_reds.get(utils.reverse_bl(key))
+            ]
+        elif key in self._mapped_reds:
+            return self._mapped_reds.get(key)
+        else:
+            raise KeyError(
+                f"Baseline {key} is not in the group of spatial redundancies"
+            )
+
+    def get_pol(self, pol):
+        """Get all radially redundant groups with a given polarization"""
+        for group in self:
+            if group.pol == pol:
+                yield group
+
+    def filter_radial_groups(
+        self,
+        min_nbls=4,
+        bls=None,
+        ex_bls=None,
+        ants=None,
+        ex_ants=None,
+        ubls=None,
+        ex_ubls=None,
+        pols=None,
+        ex_pols=None,
+        antpos=None,
+        min_bl_cut=None,
+        max_bl_cut=None,
+    ):
+        """
+        Find all radial groups with
+
+        Parameters:
+        ----------
+            min_nbls: pass
+                pass
+            ex_bls: pass
+                pass
+            ex_ants: pass
+                pass
+        """
+        _bad_groups = []
+        for gi, group in enumerate(self._radial_groups):
+            # Filter radially redundant group
+            group.filter_group(
+                bls=bls,
+                ex_bls=ex_bls,
+                ants=ants,
+                ex_ants=ex_ants,
+                ubls=ubls,
+                ex_ubls=ex_ubls,
+                pols=pols,
+                ex_pols=ex_pols,
+                min_bl_cut=min_bl_cut,
+                max_bl_cut=max_bl_cut,
+            )
+            # Identify groups with fewer than min_nbls baselines
+            if len(group) < min_nbls:
+                _bad_groups.append(gi)
+            else:
+                pass
+
+        # Remove antennas with fewer than min_nbls
+        for _bad_group in sorted(_bad_groups, reverse=True):
+            self._radial_groups.pop(_bad_group)
+
+    def __iter__(self):
+        """Iterates through the list of redundant groups"""
+        return iter(self._radial_groups)
--- hera_cal/tests/test_nucal.py
+++ hera_cal/tests/test_nucal.py
@@ -1,89 +1,154 @@
-import pytest 
+import pytest
 import numpy as np
 from copy import deepcopy
-from hera_sim.antpos import linear_array
+from hera_sim.antpos import linear_array, hex_array
 
 from .. import redcal
 from .. import nucal
+from .. import utils
+
+
+def test_is_frequency_redundant():
+    antpos = {i: np.array([i, 0, 0]) for i in range(3)}
+    freqs = np.linspace(1, 2, 10)
+
+    # Two baselines in a linear array should be redundant
+    bl1 = (0, 1, "nn")
+    bl2 = (0, 2, "nn")
+    assert nucal.is_frequency_redundant(bl1, bl2, freqs, antpos)
+
+    # One baseline should technically be frequency redundant with itself
+    assert nucal.is_frequency_redundant(bl1, bl1, freqs, antpos)
+
+    # Narrowing the bandwidth should make the baselines not frequency redundant
+    freqs = np.linspace(0.5, 0.6, 10)
+    assert not nucal.is_frequency_redundant(bl1, bl2, freqs, antpos)
+
+    # Orthogonal baselines should not be frequency redundant
+    bl1 = (0, 1, "nn")
+    bl2 = (0, 2, "nn")
+    antpos = {0: np.array([0, 0, 0]), 1: np.array([1, 0, 0]), 2: np.array([0, 1, 0])}
+    assert not nucal.is_frequency_redundant(bl1, bl2, freqs, antpos)
+
+
+def test_get_unique_orientations():
+    antpos = linear_array(7)
+    radial_groups = nucal.get_unique_orientations(antpos)
+
+    # Linear arrays should only have one unique orientation
+    assert len(radial_groups) == 1
+
+    # Give spatial redundancies to function and confirm result is the same
+    reds = redcal.get_reds(antpos)
+    _radial_groups = nucal.get_unique_orientations(antpos, reds)
+    assert len(radial_groups) == len(_radial_groups)
+    assert len(radial_groups[0]) == len(_radial_groups[0])
+
+    # Check to see if function identifies flipped orientations
+    reds[1] = [utils.reverse_bl(bls) for bls in reds[1]]
+    print(reds)
+    radial_groups = nucal.get_unique_orientations(antpos, reds, pols=["nn", "ee"])
+    assert len(radial_groups) == 1
+
+    # If multiple polarizations are requested, list should be size 2
+    radial_groups = nucal.get_unique_orientations(antpos, pols=["nn", "ee"])
+    assert len(radial_groups) == 2
+
+    # Filter by minimum number of baselines in unique orientation
+    antpos = hex_array(4, outriggers=0, split_core=False)
+    radial_groups = nucal.get_unique_orientations(
+        antpos, pols=["nn"], min_ubl_per_orient=5
+    )
+    for group in radial_groups:
+        assert len(group) >= 5
+
 
 class TestRadialRedundantGroup:
     def setup(self):
         self.antpos = linear_array(6)
-        self.pols = ['nn']
+        self.pols = ["nn"]
         self.reds = redcal.get_reds(self.antpos, pols=self.pols)
-        
+
     def test_init(self):
         # Select group from reds
         radial_group = nucal.RadialRedundantGroup(self.reds[0], self.antpos)
         assert radial_group.pol == self.pols[0]
-        radial_group = nucal.RadialRedundantGroup(self.reds[0], self.antpos, pol='nn')
+        radial_group = nucal.RadialRedundantGroup(self.reds[0], self.antpos, pol="nn")
         assert radial_group.pol == self.pols[0]
-        radial_group = nucal.RadialRedundantGroup(self.reds[0], self.antpos, blvec=np.array([1, 0, 0]))
+        radial_group = nucal.RadialRedundantGroup(
+            self.reds[0], self.antpos, blvec=np.array([1, 0, 0])
+        )
         assert np.allclose(radial_group.blvec, np.array([1, 0, 0]))
-        
+
         for bls in radial_group:
             ant1, ant2, pol = bls
-            blvec = (self.antpos[ant2] - self.antpos[ant1]) / np.linalg.norm(self.antpos[ant2] - self.antpos[ant1])
+            blvec = (self.antpos[ant2] - self.antpos[ant1]) / np.linalg.norm(
+                self.antpos[ant2] - self.antpos[ant1]
+            )
             assert np.allclose(radial_group.blvec, blvec)
-            
+
         assert len(radial_group) == len(self.reds[0])
-        
+
         baseline_lengths = []
         for bls in self.reds[0]:
             ant1, ant2, pol = bls
-            baseline_lengths.append(np.linalg.norm(self.antpos[ant2] - self.antpos[ant1]))
-            
+            baseline_lengths.append(
+                np.linalg.norm(self.antpos[ant2] - self.antpos[ant1])
+            )
+
         assert np.allclose(radial_group.baseline_lengths, np.sort(baseline_lengths))
-        
+
         antpos = linear_array(4)
-        pols = ['nn', 'ee']
+        pols = ["nn", "ee"]
         reds = redcal.get_reds(antpos, pols=pols)
         baselines = sum(reds, [])
         pytest.raises(ValueError, nucal.RadialRedundantGroup, baselines, antpos)
-        
+
     def test_get_u_bounds(self):
         freqs = np.linspace(50e6, 250e6, 10)
         umodes = []
         for bls in self.reds[0]:
             ant1, ant2, pol = bls
-            umodes.append(freqs * np.linalg.norm(self.antpos[ant2] - self.antpos[ant1]) / 2.998e8)
-            
+            umodes.append(
+                freqs * np.linalg.norm(self.antpos[ant2] - self.antpos[ant1]) / 2.998e8
+            )
+
         umin, umax = np.min(umodes), np.max(umodes)
         radial_group = nucal.RadialRedundantGroup(self.reds[0], self.antpos)
         _umin, _umax = radial_group.get_u_bounds(freqs)
         assert np.isclose(umin, _umin)
         assert np.isclose(umax, _umax)
-        
+
     def test_filter_groups(self):
         radial_group = nucal.RadialRedundantGroup(self.reds[0], self.antpos)
-        
+
         # Minimum baseline length cut
         rg = deepcopy(radial_group)
         rg.filter_group(min_bl_cut=20)
         for blmag in rg.baseline_lengths:
             assert blmag > 20
-        
+
         # Max baseline length cut
         rg = deepcopy(radial_group)
         rg.filter_group(max_bl_cut=50)
         for blmag in rg.baseline_lengths:
             assert blmag < 50
-            
+
         # Filter by antenna number
         rg = deepcopy(radial_group)
-        rg.filter_group(ex_ants=(0, 'Jnn'))
+        rg.filter_group(ex_ants=(0, "Jnn"))
         for bls in rg:
             assert 0 not in bls
-            
+
         # Filter all baselines by min_bl_cut
         rg = deepcopy(radial_group)
         rg.filter_group(min_bl_cut=1000)
         assert len(rg) == 0
         assert len(rg.baseline_lengths) == 0
-        
+
         # Filter all baselines by ex_ants
         rg = deepcopy(radial_group)
-        rg.filter_group(ex_ants=[(i, 'Jnn') for i in range(6)])
+        rg.filter_group(ex_ants=[(i, "Jnn") for i in range(6)])
         assert len(rg) == 0
         assert len(rg.baseline_lengths) == 0
 
@@ -94,25 +159,47 @@ class TestRadialRedundantGroup:
             assert len(radial_group[i]) == 3
 
 
+class TestFrequencyRedundancy:
+    def setup(self):
+        self.antpos = hex_array(4, outriggers=0, split_core=False)
+        self.freq_reds = nucal.FrequencyRedundancy(self.antpos)
 
-def test_is_frequency_redundant():
-    antpos = {i: np.array([i, 0, 0]) for i in range(3)}
-    freqs = np.linspace(1, 2, 10)
-    
-    # Two baselines in a linear array should be redundant
-    bl1 = (0, 1, 'nn')
-    bl2 = (0, 2, 'nn')
-    assert nucal.is_frequency_redundant(bl1, bl2, freqs, antpos)
-    
-    # One baseline should technically be frequency redundant with itself
-    assert nucal.is_frequency_redundant(bl1, bl1, freqs, antpos)
-    
-    # Narrowing the bandwidth should make the baselines not frequency redundant
-    freqs = np.linspace(0.5, 0.6, 10)
-    assert not nucal.is_frequency_redundant(bl1, bl2, freqs, antpos)
-    
-    # Orthogonal baselines should not be frequency redundant
-    bl1 = (0, 1, 'nn')
-    bl2 = (0, 2, 'nn')
-    antpos = {0: np.array([0, 0, 0]), 1: np.array([1, 0, 0]), 2: np.array([0, 1, 0])}
-    assert not nucal.is_frequency_redundant(bl1, bl2, freqs, antpos)
+    def test_init(self):
+        pass
+
+    def test_filter_groups(self):
+        freq_reds = deepcopy(self.freq_reds)
+
+        # Make sure all groups have at least 4 baselines
+        freq_reds.filter_radial_groups(min_nbls=2)
+        for group in freq_reds:
+            assert len(group) >= 2
+
+        # Filter out antenna (0, "Jnn")
+        freq_reds = deepcopy(self.freq_reds)
+        freq_reds.filter_radial_groups(ex_ants=[(0, "Jnn")])
+        for group in freq_reds:
+            for bls in group:
+                assert 0 not in bls
+
+    def test_get_pol(self):
+        freq_reds = nucal.FrequencyRedundancy(self.antpos, pols=["nn", "ee"])
+        for group in self.freq_reds.get_pol("nn"):
+            assert group.pol == "nn"
+
+    def test_get_radial_group(self):
+        reds = redcal.get_reds(self.antpos)
+        for red in reds:
+            group = self.freq_reds.get_radial_group(red[0])
+            assert red[0] in group
+
+    def test_get_redundant_group(self):
+        # Loop through the redundancies and confirm that we can grab spatially redundant groups
+        reds = redcal.get_reds(self.antpos)
+        for red in reds:
+            for bls in red:
+                group = self.freq_reds.get_redundant_group(bls)
+                assert bls in group
+
+        # Make sure KeyError is raised if incorrect key not given
+        pytest.raises(KeyError, self.freq_reds.get_redundant_group, (-1, -1, "nn"))
