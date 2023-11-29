b8d802652cabf19d150838312cbae0368a8d8d26
Steven Murray <steven.g.murray@asu.edu>
2023-11-29 09:50:09 +0100

    testfix: expect nsamples=0 for totally flagged lst/freq/pol

--- hera_cal/tests/test_lstbin_simple.py
+++ hera_cal/tests/test_lstbin_simple.py
@@ -624,13 +624,11 @@ class Test_LSTAverage:
         )
 
         assert np.all(flg_n)
-        # just because we're flagging it, doesn't mean we need to set nsamples=0
-        # or the std to inf. We have info there, we're just choosing not to use it.
-        assert np.all(norm_n == 2)
-        assert not np.any(np.isinf(std_n))
+        # nsamples is zero because all are flagged.
+        assert np.all(norm_n == 0)
+        assert np.all(np.isinf(std_n))
 
         # this time, only one column is flagged too much...
-        # this time, there's enough samples, but too many are flagged...
         flags[:] = False
         flags[:5, 0] = True
         data_n, flg_n, std_n, norm_n, db = lstbin_simple.lst_average(
@@ -642,8 +640,8 @@ class Test_LSTAverage:
         )
 
         assert np.all(flg_n[0])
-        assert np.all(norm_n[0] == 2)
-        assert not np.any(np.isinf(std_n[0]))
+        assert np.all(norm_n[0] == 0)
+        assert np.all(np.isinf(std_n[0]))
 
         assert not np.any(flg_n[1:])
         assert np.all(norm_n[1:] == 7)
