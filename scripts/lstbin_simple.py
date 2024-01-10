38d1373cdfe0975aea859e839cebdd8782fc97f5
Steven Murray <steven.g.murray@asu.edu>
2024-01-10 13:45:15 +0100

    fix: typo in lstbin_simple CLI

--- scripts/lstbin_simple.py
+++ scripts/lstbin_simple.py
@@ -60,7 +60,7 @@ kwargs['golden_lsts'] = tuple(float(lst) for lst in kwargs['golden_lsts'].split(
 
 kwargs['output_flagged'] = not kwargs.pop('no_flagged_mode')
 kwargs['output_inpainted'] = kwargs.pop("do_inpaint_mode")
-kwargs['sigma_clip_subbands'] = [int(b) for b in kwargs.pop["sigma_clip_subbands"].split(",")]
+kwargs['sigma_clip_subbands'] = [int(b) for b in kwargs["sigma_clip_subbands"].split(",")]
 
 run_with_profiling(
     lstbin.lst_bin_files,
