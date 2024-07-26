fix noise calculation for inpainting

hera_cal/lst_stack/averaging.py
@@ -703,7 +703,11 @@ def average_and_inpaint_simultaneously(
     )
 
     # Map polarizations to pol indices for correct noise variance computation
-    pol_to_idx = ...
+    pol_to_idx = {}
+    for polidx, pol in enumerate(stack.pols):
+        pol1, pol2 = utils.split_pol(pol)
+        if pol1 == pol2:
+            pol_to_idx[pol] = polidx
 
     # Compute noise variance
     if auto_stack.data.shape[1] != 1:
@@ -734,9 +738,10 @@ def average_and_inpaint_simultaneously(
             flagged_mean = lstavg["data"][iap, :, polidx]
             avg_flgs = lstavg["flags"][iap, :, polidx]
 
+            pol1, pol2 = utils.split_pol(pol)
             # Compute noise variance for all days in stack
             base_noise_var = (
-                np.abs(auto_stack.data[:, 0, :, polidx]) ** 2
+                np.abs(auto_stack.data[:, 0, :, pol_to_idx[pol1]] * auto_stack.data[:, 0, :, pol_to_idx[pol2]])
                 / (stack.dt * stack.df).value
             )
 