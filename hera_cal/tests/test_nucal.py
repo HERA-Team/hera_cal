d16533643378ac93c9cd2a9ef5c54b57c4a3a7d2
Tyler Cox <tyler.a.cox@berkeley.edu>
2025-01-29 10:09:43 -0800

    add test with cache to _linear_fit

--- hera_cal/tests/test_nucal.py
+++ hera_cal/tests/test_nucal.py
@@ -425,9 +425,8 @@ def test_linear_fit():
     np.testing.assert_allclose(b1, b1_cached)
     b2, _ = nucal._linear_fit(XTX, Xy, solver='solve')
     b3, _ = nucal._linear_fit(XTX, Xy, solver='lstsq')
-    b4, cached_input = nucal._linear_fit(XTX, Xy, solver='pinv')
+    b4, _ = nucal._linear_fit(XTX, Xy, solver='pinv')
     b5, _ = nucal._linear_fit(XTX, Xy, solver='cho_solve')
-    assert cached_input.get('XTXinv') is not None
 
     # Show that all modes give the same result
     np.testing.assert_allclose(b1, b2, atol=1e-6)
@@ -446,6 +445,11 @@ def test_linear_fit():
     # Test that an error is raised if the tolerance is negative
     with pytest.raises(AssertionError):
         b = nucal._linear_fit(XTX, Xy, alpha=-1)
+
+    for mode in ['lu_solve', 'cho_solve', 'pinv', 'lstsq', 'solve']:
+        b, cached_input = nucal._linear_fit(XTX, Xy, solver=mode)
+        b_cached, _ = nucal._linear_fit(XTX, Xy, solver=mode, cached_input=cached_input)
+        np.testing.assert_allclose(b, b_cached)
 
 
 def test_compute_spectral_filters():
