Fix phase reference calculation in polfilt.py

hera_cal/polfilt.py
@@ -303,10 +303,9 @@ def fit_polarized_source_model_single_bl(
 
         # Geometric phase toward this source
         lmn = lmns[si][:, time_slice]
-        lmn_phaseref = np.array([lmn[0], lmn[1], lmn[2] - 1.0])
         phasor = np.exp(
             2j * np.pi
-            * np.dot(blvec, lmn_phaseref)[:, None]
+            * np.dot(blvec, lmn)[:, None]
             * freqs[None]
             / SPEED_OF_LIGHT
         )